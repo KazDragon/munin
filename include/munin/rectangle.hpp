4c6c64126b83599c201924a66247185cc4eed651
KazDragon <matthew.chaplain@gmail.com>
2017-02-10 22:01:56 +0100

    Added skeleton for tests.

--- include/munin/rectangle.hpp
+++ include/munin/rectangle.hpp
@@ -1,36 +1,8 @@
-// ==========================================================================
-// Munin Rectangle.
-//
-// Copyright (C) 2015 Matthew Chaplain, All Rights Reserved.
-//
-// Permission to reproduce, distribute, perform, display, and to prepare
-// derivitive works from this file under the following conditions:
-//
-// 1. Any copy, reproduction or derivitive work of any part of this file 
-//    contains this copyright notice and licence in its entirety.
-//
-// 2. The rights granted to you under this license automatically terminate
-//    should you attempt to assert any patent claims against the licensor 
-//    or contributors, which in any way restrict the ability of any party 
-//    from using this software or portions thereof in any form under the
-//    terms of this license.
-//
-// Disclaimer: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
-//             KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
-//             WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-//             PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
-//             OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
-//             OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-//             OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
-//             SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
-// ==========================================================================
-#ifndef MUNIN_RECTANGLE_HPP_
-#define MUNIN_RECTANGLE_HPP_
+#pragma once
 
 #include "munin/export.hpp"
 #include <terminalpp/point.hpp>
 #include <terminalpp/extent.hpp>
-#include <iosfwd>
 
 namespace munin {
 
@@ -86,13 +58,4 @@ constexpr bool operator!=(rectangle const &lhs, rectangle const &rhs)
     return !(lhs == rhs);
 }
 
-// ==========================================================================
-// OPERATOR<<(RECTANGLE)
-// ==========================================================================
-MUNIN_EXPORT
-std::ostream& operator<<(std::ostream &out, rectangle const &rect);
-
-
 }
-
-#endif
