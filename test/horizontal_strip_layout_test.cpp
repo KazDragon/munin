29da3ef28990e9d8ddb89a67061ce3551b8bb64c
KazDragon <matthew.chaplain@gmail.com>
2017-02-28 07:14:18 +0000

    Added extra tests for horizontal_strip_layout.

--- test/horizontal_strip_layout_test.cpp
+++ test/horizontal_strip_layout_test.cpp
@@ -30,3 +30,30 @@ TEST(a_horizontal_strip_layout_with_one_component, has_a_preferred_size_of_that_
     
     ASSERT_EQ(comp_preferred_size, preferred_size);
 }
+
+TEST(
+    a_horizontal_strip_layout_with_multiple_components, 
+    has_a_preferred_size_with_the_width_of_the_widest_and_height_of_the_sum_of_the_heights)
+{
+    static constexpr terminalpp::extent comp0_preferred_size{80, 24};
+    static constexpr terminalpp::extent comp1_preferred_size{60, 13};
+    static constexpr terminalpp::extent expected_preferred_size{80, 37};
+    
+    auto comp0 = std::make_shared<StrictMock<component>>();
+    EXPECT_CALL(*comp0, do_get_preferred_size())
+        .WillOnce(Return(comp0_preferred_size));
+        
+    auto comp1 = std::make_shared<StrictMock<component>>();
+    EXPECT_CALL(*comp1, do_get_preferred_size())
+        .WillOnce(Return(comp1_preferred_size));
+        
+    std::shared_ptr<munin::component> components[] = {
+        std::shared_ptr<munin::component>(comp0),
+        std::shared_ptr<munin::component>(comp1)
+    };
+    
+    munin::horizontal_strip_layout hsl;
+    auto preferred_size = hsl.get_preferred_size(components, {});
+    
+    ASSERT_EQ(expected_preferred_size, preferred_size);
+}
