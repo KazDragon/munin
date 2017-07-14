8ae663143e1a61989281e5c8ac5d21b67d8ad879
KazDragon <matthew.chaplain@gmail.com>
2017-07-14 06:05:50 +0000

    Implemented Vertical Strip Layout

---
+++ include/munin/vertical_strip_layout.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "munin/layout.hpp"
+#include <boost/shared_ptr.hpp>
+
+namespace munin {
+
+
+//* =========================================================================
+/// \brief A class that knows how to lay components out in a container in
+/// vertical strips.  Each component is given all the available height,
+/// with the sum of the preferred widths of all the components.
+///
+/// +----------+
+/// |+-+-----+ |
+/// || |     | |
+/// || |     | |
+/// || |     | |
+/// || |     | |
+/// |+-+-----+ |
+/// +----------+
+//* =========================================================================
+class MUNIN_EXPORT vertical_strip_layout
+    : public layout
+{
+protected :
+    //* =====================================================================
+    /// \brief Called by get_preferred_size().  Derived classes must override
+    /// this function in order to retrieve the preferred size of the layout
+    /// in a custom manner.
+    //* =====================================================================
+    terminalpp::extent do_get_preferred_size(
+        std::vector<std::shared_ptr<component>> const &components,
+        std::vector<boost::any>                 const &hints) const override;
+
+    //* =====================================================================
+    /// \brief Called by operator().  Derived classes must override this
+    /// function in order to lay a container's components out in a custom
+    /// manner.
+    //* =====================================================================
+    void do_layout(
+        std::vector<std::shared_ptr<component>> const &components,
+        std::vector<boost::any>                 const &hints,
+        terminalpp::extent                           size) const override;
+};
+
+//* =========================================================================
+/// \brief Returns a newly created vertical strip layout
+//* =========================================================================
+MUNIN_EXPORT
+std::unique_ptr<layout> make_vertical_strip_layout();
+
+}
