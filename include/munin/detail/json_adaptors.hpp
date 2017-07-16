ab54fc9cd1b1655eb8187d5a1502a89e4ff90b5e
KazDragon <matthew.chaplain@gmail.com>
2017-07-16 09:45:40 +0200

    Added JSON output for filled_box

--- include/munin/detail/json_adaptors.hpp
+++ include/munin/detail/json_adaptors.hpp
@@ -1,24 +1,17 @@
 #pragma once
-#include <terminalpp/point.hpp>
-#include <terminalpp/extent.hpp>
 #include <json.hpp>
+
+namespace terminalpp {
+    class point;
+    class extent;
+    class glyph;
+    class attribute;
+    class element;
+}
 
 namespace munin { namespace detail {
 
-nlohmann::json to_json(terminalpp::point const &pt)
-{
-    return {
-        { "x", pt.x },
-        { "y", pt.y }
-    };
-}
-
-nlohmann::json to_json(terminalpp::extent const &ext)
-{
-    return {
-        { "width", ext.width },
-        { "height", ext.height }
-    };
-}
+nlohmann::json to_json(terminalpp::point const &pt);
+nlohmann::json to_json(terminalpp::extent const &ext);
 
 }}
