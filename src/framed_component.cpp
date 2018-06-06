06d3c200073ef903c7940a0e93df667d79994601
KazDragon <matthew.chaplain@gmail.com>
2018-06-06 16:12:22 +0000

    Implemented layout functions for framed components.

--- src/framed_component.cpp
+++ src/framed_component.cpp
@@ -1,11 +1,81 @@
 #include "munin/framed_component.hpp"
+#include "munin/grid_layout.hpp"
 
 namespace munin {
+
+namespace {
+
+class framed_component_layout : public munin::layout
+{
+protected :
+    //* =====================================================================
+    /// \brief Called by get_preferred_size().  Derived classes must override
+    /// this function in order to retrieve the preferred size of the layout
+    /// in a custom manner.
+    //* =====================================================================
+    terminalpp::extent do_get_preferred_size(
+        std::vector<std::shared_ptr<component>> const &components,
+        std::vector<boost::any>                 const &hints) const override
+    {
+        return components[0]->get_preferred_size()
+             + components[1]->get_preferred_size();
+    }
+
+    //* =====================================================================
+    /// \brief Called by operator().  Derived classes must override this
+    /// function in order to lay a container's components out in a custom
+    /// manner.
+    //* =====================================================================
+    void do_layout(
+        std::vector<std::shared_ptr<component>> const &components,
+        std::vector<boost::any>                 const &hints,
+        terminalpp::extent                             size) const override
+    {
+        auto const &frame = components[0];
+        frame->set_position({0, 0});
+        frame->set_size(size);
+        
+        auto const &inner = components[1];
+        inner->set_position({
+            std::min(size.width, 1),
+            std::min(size.height, 1)
+        });
+
+        inner->set_size({
+            size.width == 0 ? 0
+          : size.width == 1 ? 1
+          : size.width - 2,
+            size.height == 0 ? 0
+          : size.height == 1 ? 1
+          : size.height - 2
+        });
+    }
+
+    //* =====================================================================
+    /// \brief Called by to_json().  Derived classes must override this
+    /// function in order to add additional data about their implementation
+    /// in a custom manner.
+    //* =====================================================================
+    nlohmann::json do_to_json() const override
+    {
+        return {};
+    }
+};
+
+std::unique_ptr<layout> make_framed_component_layout()
+{
+    return std::unique_ptr<layout>(new framed_component_layout);
+}
+
+}
 
 framed_component::framed_component(
     std::shared_ptr<component> const &frame,
     std::shared_ptr<component> const &inner_component)
 {
+    add_component(frame);
+    add_component(inner_component);
+    set_layout(make_framed_component_layout());
 }
 
 framed_component::~framed_component()
--- test/src/framed_component/framed_component_test.cpp
+++ test/src/framed_component/framed_component_test.cpp
@@ -1,7 +1,10 @@
-#include <gtest/gtest.h>
+#include "mock/component.hpp"
 #include <munin/filled_box.hpp>
 #include <munin/framed_component.hpp>
 #include <munin/solid_frame.hpp>
+#include <gtest/gtest.h>
+
+using testing::Return;
 
 TEST(a_new_framed_component, is_constructed_from_a_frame_and_a_component)
 {
@@ -9,4 +12,101 @@ TEST(a_new_framed_component, is_constructed_from_a_frame_and_a_component)
         munin::make_solid_frame(),
         munin::make_fill('X')
     );
+}
+
+TEST(a_zero_size_framed_component, positions_subcomponents_at_origin)
+{
+    std::shared_ptr<mock_component> mock_frame = make_mock_component();
+    std::shared_ptr<mock_component> mock_comp  = make_mock_component();
+    
+    auto framed_component = munin::make_framed_component(
+        mock_frame,
+        mock_comp);
+        
+    EXPECT_CALL(*mock_frame, do_set_position(terminalpp::point{0, 0}));
+    EXPECT_CALL(*mock_frame, do_set_size(terminalpp::extent{0, 0}));
+    EXPECT_CALL(*mock_comp,  do_set_position(terminalpp::point{0, 0}));
+    EXPECT_CALL(*mock_comp,  do_set_size(terminalpp::extent{0, 0}));
+    
+    framed_component->set_position({0, 0});
+    framed_component->set_size({0, 0});
+}
+
+TEST(a_zero_width_framed_component, positions_inner_component_in_an_inner_row)
+{
+    // Despite it also being a zero-width component, the frame should be
+    // respected as much as possible when positioning the inner component.
+
+    std::shared_ptr<mock_component> mock_frame = make_mock_component();
+    std::shared_ptr<mock_component> mock_comp  = make_mock_component();
+    
+    auto framed_component = munin::make_framed_component(
+        mock_frame,
+        mock_comp);
+        
+    EXPECT_CALL(*mock_frame, do_set_position(terminalpp::point{0, 0}));
+    EXPECT_CALL(*mock_frame, do_set_size(terminalpp::extent{0, 5}));
+    EXPECT_CALL(*mock_comp,  do_set_position(terminalpp::point{0, 1}));
+    EXPECT_CALL(*mock_comp,  do_set_size(terminalpp::extent{0, 3}));
+    
+    framed_component->set_position({0, 0});
+    framed_component->set_size({0, 5});
+}
+
+TEST(a_zero_height_framed_component, positions_inner_component_in_an_inner_column)
+{
+    // Despite it also being a zero-height component, the frame should be
+    // respected as much as possible when positioning the inner component.
+
+    std::shared_ptr<mock_component> mock_frame = make_mock_component();
+    std::shared_ptr<mock_component> mock_comp  = make_mock_component();
+    
+    auto framed_component = munin::make_framed_component(
+        mock_frame,
+        mock_comp);
+        
+    EXPECT_CALL(*mock_frame, do_set_position(terminalpp::point{0, 0}));
+    EXPECT_CALL(*mock_frame, do_set_size(terminalpp::extent{5, 0}));
+    EXPECT_CALL(*mock_comp,  do_set_position(terminalpp::point{1, 0}));
+    EXPECT_CALL(*mock_comp,  do_set_size(terminalpp::extent{3, 0}));
+    
+    framed_component->set_position({0, 0});
+    framed_component->set_size({5, 0});
+}
+
+TEST(a_framed_component_larger_than_the_frame, positions_the_inner_component_inward_of_the_frame)
+{
+    std::shared_ptr<mock_component> mock_frame = make_mock_component();
+    std::shared_ptr<mock_component> mock_comp  = make_mock_component();
+    
+    auto framed_component = munin::make_framed_component(
+        mock_frame,
+        mock_comp);
+        
+    EXPECT_CALL(*mock_frame, do_set_position(terminalpp::point{0, 0}));
+    EXPECT_CALL(*mock_frame, do_set_size(terminalpp::extent{5, 7}));
+    EXPECT_CALL(*mock_comp,  do_set_position(terminalpp::point{1, 1}));
+    EXPECT_CALL(*mock_comp,  do_set_size(terminalpp::extent{3, 5}));
+    
+    framed_component->set_position({0, 0});
+    framed_component->set_size({5, 7});
+}
+
+TEST(a_frame_component, has_a_preferred_size_of_the_sum_of_the_frame_and_inner)
+{
+    std::shared_ptr<mock_component> mock_frame = make_mock_component();
+    std::shared_ptr<mock_component> mock_comp  = make_mock_component();
+    
+    auto framed_component = munin::make_framed_component(
+        mock_frame,
+        mock_comp);
+        
+    EXPECT_CALL(*mock_frame, do_get_preferred_size())
+        .WillOnce(Return(terminalpp::extent(2, 2)));
+    EXPECT_CALL(*mock_comp, do_get_preferred_size())
+        .WillOnce(Return(terminalpp::extent(7, 13)));
+
+    ASSERT_EQ(
+        terminalpp::extent(9, 15),
+        framed_component->get_preferred_size());
 }
