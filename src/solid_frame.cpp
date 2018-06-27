8f91e5c5aed1572ec5984705384d2f80fbea6de7
Matthew Chaplain <matthew.chaplain@gmail.com>
2018-06-27 12:33:45 +0200

    Solid Frame can now use unicode characters for drawing.

--- src/solid_frame.cpp
+++ src/solid_frame.cpp
@@ -1,9 +1,78 @@
-#include <munin/solid_frame.hpp>
-#include <munin/compass_layout.hpp>
-#include <munin/filled_box.hpp>
-#include <munin/view.hpp>
+#include "munin/solid_frame.hpp"
+#include "munin/detail/unicode_glyphs.hpp"
+#include "munin/compass_layout.hpp"
+#include "munin/filled_box.hpp"
+#include "munin/graphics.hpp"
+#include "munin/view.hpp"
 
 namespace munin {
+namespace {
+
+constexpr terminalpp::glyph const default_corner_glyph          = '+';
+constexpr terminalpp::glyph const default_horizontal_beam_glyph = '-';
+constexpr terminalpp::glyph const default_vertical_beam_glyph   = '|';
+    
+// ==========================================================================
+// TOP_LEFT_CORNER_GLYPH
+// ==========================================================================
+terminalpp::glyph top_left_corner_glyph()
+{
+    return get_graphics().supports_unicode()
+         ? detail::double_lined_top_left_corner
+         : default_corner_glyph;
+}
+
+// ==========================================================================
+// TOP_RIGHT_CORNER_GLYPH
+// ==========================================================================
+terminalpp::glyph top_right_corner_glyph()
+{
+    return get_graphics().supports_unicode()
+         ? detail::double_lined_top_right_corner
+         : default_corner_glyph;
+}
+
+// ==========================================================================
+// BOTTOM_LEFT_CORNER_GLYPH
+// ==========================================================================
+terminalpp::glyph bottom_left_corner_glyph()
+{
+    return get_graphics().supports_unicode()
+         ? detail::double_lined_bottom_left_corner
+         : default_corner_glyph;
+}
+
+// ==========================================================================
+// BOTTOM_RIGHT_CORNER_GLYPH
+// ==========================================================================
+terminalpp::glyph bottom_right_corner_glyph()
+{
+    return get_graphics().supports_unicode()
+         ? detail::double_lined_bottom_right_corner
+         : default_corner_glyph;
+}
+
+// ==========================================================================
+// HORIZONTAL_BEAM_GLYPH
+// ==========================================================================
+terminalpp::glyph horizontal_beam_glyph()
+{
+    return get_graphics().supports_unicode()
+         ? detail::double_lined_horizontal_beam
+         : default_horizontal_beam_glyph;
+}
+
+// ==========================================================================
+// VERTICAL_BEAM_GLYPH
+// ==========================================================================
+terminalpp::glyph vertical_beam_glyph()
+{
+    return get_graphics().supports_unicode()
+         ? detail::double_lined_vertical_beam
+         : default_vertical_beam_glyph;
+}
+
+}
 
 struct solid_frame::impl
 {
@@ -13,26 +82,22 @@ struct solid_frame::impl
         terminalpp::colour(),
         terminalpp::ansi::graphics::intensity::bold};
 
-    static constexpr terminalpp::glyph corner_glyph          = '+';
-    static constexpr terminalpp::glyph horizontal_beam_glyph = '-';
-    static constexpr terminalpp::glyph vertical_beam_glyph   = '|';
-    
     std::shared_ptr<munin::filled_box> top_left = 
-        munin::make_fill({corner_glyph, lowlight_attribute});
+        munin::make_fill({top_left_corner_glyph(), lowlight_attribute});
     std::shared_ptr<munin::filled_box> top_centre = 
-        munin::make_fill({horizontal_beam_glyph, lowlight_attribute});
+        munin::make_fill({horizontal_beam_glyph(), lowlight_attribute});
     std::shared_ptr<munin::filled_box> top_right = 
-        munin::make_fill({corner_glyph, lowlight_attribute});
+        munin::make_fill({top_right_corner_glyph(), lowlight_attribute});
     std::shared_ptr<munin::filled_box> centre_left = 
-        munin::make_fill({vertical_beam_glyph, lowlight_attribute});
+        munin::make_fill({vertical_beam_glyph(), lowlight_attribute});
     std::shared_ptr<munin::filled_box> centre_right = 
-        munin::make_fill({vertical_beam_glyph, lowlight_attribute});
+        munin::make_fill({vertical_beam_glyph(), lowlight_attribute});
     std::shared_ptr<munin::filled_box> bottom_left = 
-        munin::make_fill({corner_glyph, lowlight_attribute});
+        munin::make_fill({bottom_left_corner_glyph(), lowlight_attribute});
     std::shared_ptr<munin::filled_box> bottom_centre = 
-        munin::make_fill({horizontal_beam_glyph, lowlight_attribute});
+        munin::make_fill({horizontal_beam_glyph(), lowlight_attribute});
     std::shared_ptr<munin::filled_box> bottom_right = 
-        munin::make_fill({corner_glyph, lowlight_attribute});
+        munin::make_fill({bottom_right_corner_glyph(), lowlight_attribute});
     
     bool is_highlighted = false;
     
@@ -40,21 +105,20 @@ struct solid_frame::impl
     {
         auto const &attr = is_highlighted ? highlight_attribute : lowlight_attribute;
         
-        top_left->set_fill({corner_glyph, attr});
-        top_centre->set_fill({horizontal_beam_glyph, attr});
-        top_right->set_fill({corner_glyph, attr});
-        centre_left->set_fill({vertical_beam_glyph, attr});
-        centre_right->set_fill({vertical_beam_glyph, attr});
-        bottom_left->set_fill({corner_glyph, attr});
-        bottom_centre->set_fill({horizontal_beam_glyph, attr});
-        bottom_right->set_fill({corner_glyph, attr});
+        top_left->set_fill({top_left_corner_glyph(), attr});
+        top_centre->set_fill({horizontal_beam_glyph(), attr});
+        top_right->set_fill({top_right_corner_glyph(), attr});
+        centre_left->set_fill({vertical_beam_glyph(), attr});
+        centre_right->set_fill({vertical_beam_glyph(), attr});
+        bottom_left->set_fill({bottom_left_corner_glyph(), attr});
+        bottom_centre->set_fill({horizontal_beam_glyph(), attr});
+        bottom_right->set_fill({bottom_right_corner_glyph(), attr});
     }
 };
 
-constexpr terminalpp::glyph solid_frame::impl::corner_glyph;
-constexpr terminalpp::glyph solid_frame::impl::horizontal_beam_glyph;
-constexpr terminalpp::glyph solid_frame::impl::vertical_beam_glyph;
-
+// ==========================================================================
+// CONSTRUCTOR
+// ==========================================================================
 solid_frame::solid_frame()
   : pimpl_(std::make_shared<impl>())
 {
@@ -81,10 +145,16 @@ solid_frame::solid_frame()
     add_component(east_beam, compass_layout::heading::east);
 }
 
+// ==========================================================================
+// DESTRUCTOR
+// ==========================================================================
 solid_frame::~solid_frame()
 {
 }
 
+// ==========================================================================
+// SET_HIGHLIGHT_ATTRIBUTE
+// ==========================================================================
 void solid_frame::set_highlight_attribute(
     terminalpp::attribute const &highlight_attribute)
 {
@@ -92,6 +162,9 @@ void solid_frame::set_highlight_attribute(
     pimpl_->set_fills();
 }
 
+// ==========================================================================
+// SET_LOWLIGHT_ATTRIBUTE
+// ==========================================================================
 void solid_frame::set_lowlight_attribute(
     terminalpp::attribute const &lowlight_attribute)
 {
@@ -99,12 +172,18 @@ void solid_frame::set_lowlight_attribute(
     pimpl_->set_fills();
 }
 
+// ==========================================================================
+// SET_HIGHLIGHT
+// ==========================================================================
 void solid_frame::set_highlight(bool highlight)
 {
     pimpl_->is_highlighted = highlight;
     pimpl_->set_fills();
 }
 
+// ==========================================================================
+// MAKE_SOLID_FRAME
+// ==========================================================================
 std::shared_ptr<solid_frame> make_solid_frame()
 {
     return std::make_shared<solid_frame>();
--- test/src/solid_frame/solid_frame_test.cpp
+++ test/src/solid_frame/solid_frame_test.cpp
@@ -1,7 +1,12 @@
+#include "mock/graphics.hpp"
 #include <munin/solid_frame.hpp>
 #include <terminalpp/canvas.hpp>
 #include <terminalpp/canvas_view.hpp>
+#include <terminalpp/string.hpp>
 #include <gtest/gtest.h>
+
+using testing::NiceMock;
+using testing::Return;
 
 TEST(a_solid_frame, is_a_component)
 {
@@ -185,3 +190,59 @@ TEST(a_solid_frame, remains_unhighlighted_when_setting_the_highlight_attribute)
     ASSERT_EQ(terminalpp::element('-', lowlight_attribute), cv[2][3]);
     ASSERT_EQ(terminalpp::element('+', lowlight_attribute), cv[3][3]);
 }
+
+class a_solid_frame_with_unicode_graphics : public testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        ON_CALL(mock_gr_, supports_unicode())
+            .WillByDefault(Return(true));
+
+        munin::set_graphics(&mock_gr_);
+    }
+
+    void TearDown() override
+    {
+        munin::set_graphics(nullptr);
+    }
+
+    NiceMock<mock_graphics> mock_gr_;  
+};
+
+TEST_F(a_solid_frame_with_unicode_graphics, draws_a_border_with_box_drawing_glyphs)
+{
+    munin::solid_frame frame;
+    frame.set_size({4, 4});
+    
+    terminalpp::canvas canvas({4, 4});
+    terminalpp::canvas_view cv(canvas);
+    frame.draw(cv, {{}, {4, 4}});
+
+    using namespace terminalpp::literals;
+    static terminalpp::element const top_left_corner     = "\\U2554"_ets[0];
+    static terminalpp::element const top_right_corner    = "\\U2557"_ets[0];
+    static terminalpp::element const bottom_left_corner  = "\\U255A"_ets[0];
+    static terminalpp::element const bottom_right_corner = "\\U255D"_ets[0];
+    static terminalpp::element const horizontal_beam     = "\\U2550"_ets[0];
+    static terminalpp::element const vertical_beam       = "\\U2551"_ets[0];
+
+    ASSERT_EQ(top_left_corner,     terminalpp::element(cv[0][0]));
+    ASSERT_EQ(horizontal_beam,     cv[1][0]);
+    ASSERT_EQ(horizontal_beam,     cv[2][0]);
+    ASSERT_EQ(top_right_corner,    cv[3][0]);
+    ASSERT_EQ(vertical_beam,       cv[0][1]);
+    ASSERT_EQ(vertical_beam,       cv[0][2]);
+    ASSERT_EQ(vertical_beam,       cv[3][2]);
+    ASSERT_EQ(vertical_beam,       cv[3][1]);
+    ASSERT_EQ(bottom_left_corner,  cv[0][3]);
+    ASSERT_EQ(horizontal_beam,     cv[1][3]);
+    ASSERT_EQ(horizontal_beam,     cv[2][3]);
+    ASSERT_EQ(bottom_right_corner, cv[3][3]);
+}
+
+/*
+TEST(a_solid_frame, draws_a_border)
+{
+}
+*/
