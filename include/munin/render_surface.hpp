f4fe4d1845c5dc43f563d9ef3425ad248482e7fd
KazDragon <matthew.chaplain@gmail.com>
2018-07-18 05:29:52 +0000

    Created render_surface based on canvas_view.

---
+++ include/munin/render_surface.hpp
@@ -0,0 +1,185 @@
+#pragma once
+
+#include "munin/export.hpp"
+#include "munin/render_surface_capabilities.hpp"
+#include <terminalpp/element.hpp>
+#include <terminalpp/extent.hpp>
+#include <iosfwd>
+
+namespace terminalpp {
+    class canvas;
+}
+
+namespace munin {
+
+//* =========================================================================
+/// \brief A sub-view into a canvas that only allows reading and writing
+/// to a its elements; not operations that would affect the entire canvas.
+//* =========================================================================
+class MUNIN_EXPORT render_surface
+{
+public :
+    //* =====================================================================
+    /// \brief A proxy into a row of elements on the canvas
+    //* =====================================================================
+    class row_proxy
+    {
+    public :
+        // ==================================================================
+        // CONSTRUCTOR
+        // ==================================================================
+        row_proxy(
+            render_surface &surface, 
+            terminalpp::coordinate_type column, 
+            terminalpp::coordinate_type row);
+
+        // ==================================================================
+        // OPERATOR=
+        // ==================================================================
+        row_proxy &operator=(row_proxy const &other);
+
+        // ==================================================================
+        // OPERATOR=
+        // ==================================================================
+        row_proxy &operator=(terminalpp::element const &value);
+
+        // ==================================================================
+        // CONVERSION OPERATOR: ELEMENT
+        // ==================================================================
+        operator terminalpp::element &();
+
+        // ==================================================================
+        // CONVERSION OPERATOR: ELEMENT
+        // ==================================================================
+        operator terminalpp::element const &() const;
+
+        // ==================================================================
+        // EQUALITY: ELEMENT
+        // ==================================================================
+        bool operator==(const terminalpp::element& value) const;
+        
+    private :
+        render_surface &surface_;
+        terminalpp::coordinate_type column_;
+        terminalpp::coordinate_type row_;
+    };
+
+    //* =====================================================================
+    /// \brief A proxy into a column of elements on the canvas
+    //* =====================================================================
+    class column_proxy
+    {
+    public :
+        // ==================================================================
+        // CONSTRUCTOR
+        // ==================================================================
+        column_proxy(
+            render_surface &surface, 
+            terminalpp::coordinate_type column);
+
+        // ==================================================================
+        // OPERATOR[]
+        // ==================================================================
+        row_proxy operator[](terminalpp::coordinate_type row);
+
+    private :
+        render_surface &surface_;
+        terminalpp::coordinate_type column_;
+    };
+
+    //* =====================================================================
+    /// \brief A constant proxy into a column of elements on the canvas
+    //* =====================================================================
+    class const_column_proxy
+    {
+    public :
+        // ==================================================================
+        // CONSTRUCTOR
+        // ==================================================================
+        const_column_proxy(
+            render_surface const &surface, 
+            terminalpp::coordinate_type column);
+
+        // ==================================================================
+        // OPERATOR[]
+        // ==================================================================
+        terminalpp::element const &operator[](
+            terminalpp::coordinate_type row) const;
+
+    private :
+        render_surface const &surface_;
+        terminalpp::coordinate_type column_;
+    };
+
+    //* =====================================================================
+    /// \brief Constructor
+    //* =====================================================================
+    explicit render_surface(terminalpp::canvas &cvs);
+
+    //* =====================================================================
+    //\ brief Constructor with explicit render surface capabilities
+    //* =====================================================================
+    render_surface(
+        terminalpp::canvas &cvs,
+        render_surface_capabilities const &capabilities);
+
+    //* =====================================================================
+    /// \brief Returns true if the surface is known to support unicode.
+    /// characters.  Attempting to render unicode on surfaces that do not
+    /// support unicode may have unexpected results.
+    //* =====================================================================
+    bool supports_unicode() const;
+
+    //* ==== =================================================================
+    /// \brief Offsets the canvas by a certain amount, causing it to become
+    /// a view with the offset location as a basis.
+    //* =====================================================================
+    void offset_by(terminalpp::extent offset);
+
+    //* =====================================================================
+    /// \brief Returns the size of the canvas.
+    //* =====================================================================
+    terminalpp::extent size() const;
+
+    //* =====================================================================
+    /// \brief A subscript operator into a column
+    //* =====================================================================
+    column_proxy operator[](terminalpp::coordinate_type column);
+
+    //* =====================================================================
+    /// \brief A subscript operator into a column
+    //* =====================================================================
+    const_column_proxy operator[](terminalpp::coordinate_type column) const;
+
+private :
+    //* =====================================================================
+    /// \brief Set the value of an element.
+    //* =====================================================================
+    void set_element(
+        terminalpp::coordinate_type column, 
+        terminalpp::coordinate_type row, 
+        terminalpp::element const &value);
+
+    //* =====================================================================
+    /// \brief Get the value of an element.
+    //* =====================================================================
+    terminalpp::element &get_element(
+        terminalpp::coordinate_type column, 
+        terminalpp::coordinate_type row);
+
+    //* =====================================================================
+    /// \brief Get the value of an element.
+    //* =====================================================================
+    terminalpp::element const &get_element(
+        terminalpp::coordinate_type column, 
+        terminalpp::coordinate_type row) const;
+
+    render_surface_capabilities const &capabilities_;
+    terminalpp::canvas &canvas_;
+    terminalpp::extent  offset_;
+};
+
+MUNIN_EXPORT
+std::ostream &operator<<(std::ostream &out, render_surface::row_proxy const &row);
+
+}
---
+++ src/render_surface.cpp
@@ -0,0 +1,227 @@
+#include "munin/render_surface.hpp"
+#include <terminalpp/canvas.hpp>
+
+namespace munin {
+namespace {
+
+// ==========================================================================
+// DEFAULT RENDER SURFACE CAPABILITIES
+// ==========================================================================
+class default_render_surface_capabilities
+  : public render_surface_capabilities
+{
+public :
+    // ======================================================================
+    // SUPPORTS_UNICODE
+    // ======================================================================
+    bool supports_unicode() const override
+    {
+        return true;
+    }
+};
+
+static default_render_surface_capabilities default_capabilities;
+
+}
+
+// ==========================================================================
+// ROW_PROXY::CONSTRUCTOR
+// ==========================================================================
+render_surface::row_proxy::row_proxy(
+    render_surface& surface, 
+    terminalpp::coordinate_type column, 
+    terminalpp::coordinate_type row)
+  : surface_(surface),
+    column_(column),
+    row_(row)
+{
+}
+
+// ==========================================================================
+// ROW_PROXY::OPERATOR=
+// ==========================================================================
+render_surface::row_proxy &render_surface::row_proxy::operator=(
+    render_surface::row_proxy const &other)
+{
+    surface_.set_element(column_, row_, other);
+    return *this;
+}
+
+// ==========================================================================
+// ROW_PROXY::OPERATOR=
+// ==========================================================================
+render_surface::row_proxy &render_surface::row_proxy::operator=(
+    terminalpp::element const &value)
+{
+    surface_.set_element(column_, row_, value);
+    return *this;
+}
+
+// ==========================================================================
+// ROW_PROXY::CONVERSION OPERATOR
+// ==========================================================================
+render_surface::row_proxy::operator terminalpp::element &()
+{
+    return surface_.get_element(column_, row_);
+}
+
+// ==========================================================================
+// ROW_PROXY::CONVERSION OPERATOR
+// ==========================================================================
+render_surface::row_proxy::operator terminalpp::element const &() const
+{
+    return surface_.get_element(column_, row_);
+}
+
+// ==========================================================================
+// ROW_PROXY::OPERATOR==
+// ==========================================================================
+bool render_surface::row_proxy::operator==(
+    terminalpp::element const& value) const
+{
+    return surface_.get_element(column_, row_) == value;
+}
+
+// ==========================================================================
+// COLUMN_PROXY::CONSTRUCTOR
+// ==========================================================================
+render_surface::column_proxy::column_proxy(
+    render_surface &surface, 
+    terminalpp::coordinate_type column)
+  : surface_(surface),
+    column_(column)
+{
+}
+
+// ==========================================================================
+// COLUMN_PROXY::OPERATOR[]
+// ==========================================================================
+render_surface::row_proxy render_surface::column_proxy::operator[](
+    terminalpp::coordinate_type row)
+{
+    return render_surface::row_proxy(surface_, column_, row);
+}
+
+// ==========================================================================
+// CONST_COLUMN_PROXY::CONSTRUCTOR
+// ==========================================================================
+render_surface::const_column_proxy::const_column_proxy(
+    render_surface const &surface, 
+    terminalpp::coordinate_type column)
+  : surface_(surface),
+    column_(column)
+{
+}
+
+// ==========================================================================
+// CONST_COLUMN_PROXY::OPERATOR[]
+// ==========================================================================
+terminalpp::element const &render_surface::const_column_proxy::operator[](
+    terminalpp::coordinate_type row) const
+{
+    return surface_.get_element(column_, row);
+}
+
+// ==========================================================================
+// CONSTRUCTOR
+// ==========================================================================
+render_surface::render_surface(terminalpp::canvas &cvs)
+  : render_surface(cvs, default_capabilities)
+{
+}
+
+// ==========================================================================
+// CONSTRUCTOR
+// ==========================================================================
+render_surface::render_surface(
+    terminalpp::canvas &cvs,
+    render_surface_capabilities const &capabilities)
+  : canvas_(cvs),
+    capabilities_(capabilities)
+{
+}
+
+// ==========================================================================
+// SUPPORTS_UNICODE
+// ==========================================================================
+bool render_surface::supports_unicode() const
+{
+    return capabilities_.supports_unicode();
+}
+
+// ==========================================================================
+// OFFSET_BY
+// ==========================================================================
+void render_surface::offset_by(
+    terminalpp::extent offset)
+{
+    offset_ += offset;
+}
+
+// ==========================================================================
+// SIZE
+// ==========================================================================
+terminalpp::extent render_surface::size() const
+{
+    return canvas_.size() - offset_;
+}
+
+// ==========================================================================
+// OPERATOR[]
+// ==========================================================================
+render_surface::column_proxy render_surface::operator[](
+    terminalpp::coordinate_type column)
+{
+    return column_proxy(*this, column);
+}
+
+// ==========================================================================
+// OPERATOR[]
+// ==========================================================================
+render_surface::const_column_proxy render_surface::operator[](
+    terminalpp::coordinate_type column) const
+{
+    return const_column_proxy(*this, column);
+}
+
+// ==========================================================================
+// GET_ELEMENT
+// ==========================================================================
+terminalpp::element &render_surface::get_element(
+    terminalpp::coordinate_type column, terminalpp::coordinate_type row)
+{
+    return canvas_[column + offset_.width][row + offset_.height];
+}
+
+// ==========================================================================
+// GET_ELEMENT
+// ==========================================================================
+terminalpp::element const &render_surface::get_element(
+    terminalpp::coordinate_type column, 
+    terminalpp::coordinate_type row) const
+{
+    return canvas_[column + offset_.width][row + offset_.height];
+}
+
+// ==========================================================================
+// SET_ELEMENT
+// ==========================================================================
+void render_surface::set_element(
+    terminalpp::coordinate_type column, 
+    terminalpp::coordinate_type row, 
+    terminalpp::element const &value)
+{
+    canvas_[column + offset_.width][row + offset_.height] = value;
+}
+
+// ==========================================================================
+// OPERATOR<<(row_proxy)
+// ==========================================================================
+std::ostream &operator<<(
+    std::ostream &out,
+    render_surface::row_proxy const &row)
+{
+    return out << terminalpp::element(row);
+}
+
+}
