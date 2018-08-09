7ff55bec85edb4809a63113c081cb2fcc96ed31f
KazDragon <matthew.chaplain@gmail.com>
2018-08-09 05:58:51 +0000

    Made shell of titled frame as copy of solid frame

---
+++ src/titled_frame.cpp
@@ -0,0 +1,249 @@
+#include "munin/titled_frame.hpp"
+#include "munin/detail/unicode_glyphs.hpp"
+#include "munin/compass_layout.hpp"
+#include "munin/filled_box.hpp"
+#include "munin/render_surface.hpp"
+#include "munin/view.hpp"
+
+namespace munin {
+namespace {
+
+constexpr terminalpp::glyph const default_corner_element          = '+';
+constexpr terminalpp::glyph const default_horizontal_beam_element = '-';
+constexpr terminalpp::glyph const default_vertical_beam_element   = '|';
+    
+// ==========================================================================
+// TOP_LEFT_CORNER_GLYPH
+// ==========================================================================
+terminalpp::element select_top_left_corner_element(
+    render_surface const &surface,
+    terminalpp::attribute const &attr)
+{
+    return {surface.supports_unicode()
+         ? detail::single_lined_rounded_top_left_corner
+         : default_corner_element, attr};
+}
+
+// ==========================================================================
+// TOP_RIGHT_CORNER_GLYPH
+// ==========================================================================
+terminalpp::element select_top_right_corner_element(
+    render_surface const &surface,
+    terminalpp::attribute const &attr)
+{
+    return {surface.supports_unicode()
+         ? detail::single_lined_rounded_top_right_corner
+         : default_corner_element, attr};
+}
+
+// ==========================================================================
+// BOTTOM_LEFT_CORNER_GLYPH
+// ==========================================================================
+terminalpp::element select_bottom_left_corner_element(
+    render_surface const &surface,
+    terminalpp::attribute const &attr)
+{
+    return {surface.supports_unicode()
+         ? detail::single_lined_rounded_bottom_left_corner
+         : default_corner_element, attr};
+}
+
+// ==========================================================================
+// BOTTOM_RIGHT_CORNER_GLYPH
+// ==========================================================================
+terminalpp::element select_bottom_right_corner_element(
+    render_surface const &surface,
+    terminalpp::attribute const &attr)
+{
+    return {surface.supports_unicode()
+         ? detail::single_lined_rounded_bottom_right_corner
+         : default_corner_element, attr};
+}
+
+// ==========================================================================
+// HORIZONTAL_BEAM_GLYPH
+// ==========================================================================
+terminalpp::element select_horizontal_beam_element(
+    render_surface const &surface,
+    terminalpp::attribute const &attr)
+{
+    return {surface.supports_unicode()
+         ? detail::single_lined_horizontal_beam
+         : default_horizontal_beam_element, attr};
+}
+
+// ==========================================================================
+// VERTICAL_BEAM_GLYPH
+// ==========================================================================
+terminalpp::element select_vertical_beam_element(
+    render_surface const &surface,
+    terminalpp::attribute const &attr)
+{
+    return {surface.supports_unicode()
+         ? detail::single_lined_vertical_beam
+         : default_vertical_beam_element, attr};
+}
+
+}
+
+struct titled_frame::impl
+{
+    terminalpp::string title;
+    terminalpp::attribute lowlight_attribute;
+    terminalpp::attribute highlight_attribute = {
+        terminalpp::ansi::graphics::colour::cyan,
+        terminalpp::colour(),
+        terminalpp::ansi::graphics::intensity::bold};
+    terminalpp::attribute *current_attribute = &lowlight_attribute;
+
+    std::shared_ptr<munin::filled_box> top_left = munin::make_fill(
+        [this](render_surface &surface)
+        {
+            return select_top_left_corner_element(surface, *current_attribute);
+        });
+            
+    std::shared_ptr<munin::filled_box> top_centre = munin::make_fill(
+        [this](render_surface &surface)
+        {
+            return select_horizontal_beam_element(surface, *current_attribute);
+        });
+        
+    std::shared_ptr<munin::filled_box> top_right = munin::make_fill(
+        [this](render_surface &surface)
+        {
+            return select_top_right_corner_element(surface, *current_attribute);
+        });
+        
+    std::shared_ptr<munin::filled_box> centre_left = munin::make_fill(
+        [this](render_surface &surface)
+        {
+            return select_vertical_beam_element(surface, *current_attribute);
+        });
+        
+    std::shared_ptr<munin::filled_box> centre_right = munin::make_fill(
+        [this](render_surface &surface)
+        {
+            return select_vertical_beam_element(surface, *current_attribute);
+        });
+        
+    std::shared_ptr<munin::filled_box> bottom_left = munin::make_fill(
+        [this](render_surface &surface)
+        {
+            return select_bottom_left_corner_element(surface, *current_attribute);
+        });
+        
+    std::shared_ptr<munin::filled_box> bottom_centre = munin::make_fill(
+        [this](render_surface &surface)
+        {
+            return select_horizontal_beam_element(surface, *current_attribute);
+        });
+        
+    std::shared_ptr<munin::filled_box> bottom_right = munin::make_fill(
+        [this](render_surface &surface)
+        {
+            return select_bottom_right_corner_element(surface, *current_attribute);
+        });
+};
+
+// ==========================================================================
+// CONSTRUCTOR
+// ==========================================================================
+titled_frame::titled_frame(terminalpp::string const &title)
+  : pimpl_(std::make_shared<impl>())
+{
+    pimpl_->title = title;
+    
+    auto north_beam = view(
+        make_compass_layout(),
+        pimpl_->top_left,   compass_layout::heading::west,
+        pimpl_->top_centre, compass_layout::heading::centre,
+        pimpl_->top_right,  compass_layout::heading::east);
+    
+    auto south_beam = view(
+        make_compass_layout(),
+        pimpl_->bottom_left, compass_layout::heading::west,
+        pimpl_->bottom_centre, compass_layout::heading::centre,
+        pimpl_->bottom_right, compass_layout::heading::east);
+        
+    auto west_beam = pimpl_->centre_left;
+    auto east_beam = pimpl_->centre_right;
+    
+    set_layout(make_compass_layout());
+    
+    add_component(north_beam, compass_layout::heading::north);
+    add_component(south_beam, compass_layout::heading::south);
+    add_component(west_beam, compass_layout::heading::west);
+    add_component(east_beam, compass_layout::heading::east);
+}
+
+// ==========================================================================
+// DESTRUCTOR
+// ==========================================================================
+titled_frame::~titled_frame()
+{
+}
+
+// ==========================================================================
+// SET_HIGHLIGHT_ATTRIBUTE
+// ==========================================================================
+void titled_frame::set_highlight_attribute(
+    terminalpp::attribute const &highlight_attribute)
+{
+    pimpl_->highlight_attribute = highlight_attribute;
+}
+
+// ==========================================================================
+// SET_LOWLIGHT_ATTRIBUTE
+// ==========================================================================
+void titled_frame::set_lowlight_attribute(
+    terminalpp::attribute const &lowlight_attribute)
+{
+    pimpl_->lowlight_attribute = lowlight_attribute;
+}
+
+// ==========================================================================
+// HIGHLIGHT_ON_FOCUS
+// ==========================================================================
+void titled_frame::highlight_on_focus(
+    std::shared_ptr<component> const &associated_component)
+{
+    auto const evaluate_focus = 
+        [this, wp = std::weak_ptr<component>(associated_component)]
+        {
+            std::shared_ptr<component> associated_component(wp.lock());
+
+            if (associated_component)
+            {
+                pimpl_->current_attribute = associated_component->has_focus()
+                                          ? &pimpl_->highlight_attribute
+                                          : &pimpl_->lowlight_attribute;
+            }
+        };
+
+    associated_component->on_focus_set.connect(evaluate_focus);
+    associated_component->on_focus_lost.connect(evaluate_focus);
+    evaluate_focus();
+}
+
+// ==========================================================================
+// MAKE_SOLID_FRAME
+// ==========================================================================
+std::shared_ptr<titled_frame> make_titled_frame(
+    terminalpp::string const &title)
+{
+    return std::make_shared<titled_frame>(title);
+}
+
+// ==========================================================================
+// MAKE_SOLID_FRAME
+// ==========================================================================
+std::shared_ptr<titled_frame> make_titled_frame(
+    terminalpp::string const &title,
+    std::shared_ptr<component> const &associated_component)
+{
+    auto frame = make_titled_frame(title);
+    frame->highlight_on_focus(associated_component);
+    return frame;
+}
+
+}
---
+++ test/src/titled_frame/titled_frame_test.cpp
@@ -0,0 +1,268 @@
+#include "mock/component.hpp"
+#include "mock/render_surface_capabilities.hpp"
+#include <munin/titled_frame.hpp>
+#include <munin/render_surface.hpp>
+#include <terminalpp/string.hpp>
+#include <gtest/gtest.h>
+
+using testing::NiceMock;
+using testing::Return;
+
+using namespace terminalpp::literals;
+static terminalpp::glyph const top_left_corner     = '+';
+static terminalpp::glyph const top_right_corner    = '+';
+static terminalpp::glyph const bottom_left_corner  = '+';
+static terminalpp::glyph const bottom_right_corner = '+';
+static terminalpp::glyph const horizontal_beam     = '-';
+static terminalpp::glyph const vertical_beam       = '|';
+
+static terminalpp::glyph const unicode_top_left_corner     = "\\U256D"_ets[0].glyph_;
+static terminalpp::glyph const unicode_top_right_corner    = "\\U256E"_ets[0].glyph_;
+static terminalpp::glyph const unicode_bottom_left_corner  = "\\U2570"_ets[0].glyph_;
+static terminalpp::glyph const unicode_bottom_right_corner = "\\U256F"_ets[0].glyph_;
+static terminalpp::glyph const unicode_horizontal_beam     = "\\U2500"_ets[0].glyph_;
+static terminalpp::glyph const unicode_vertical_beam       = "\\U2502"_ets[0].glyph_;
+
+static auto const highlight_attribute = terminalpp::attribute(
+    terminalpp::ansi::graphics::colour::cyan,
+    terminalpp::colour(),
+    terminalpp::ansi::graphics::intensity::bold);
+
+class a_titled_frame_with_no_unicode_support : public testing::Test
+{
+protected :
+    void SetUp() override
+    {
+        ON_CALL(surface_capabilities_, supports_unicode())
+            .WillByDefault(Return(false));
+    }
+
+    NiceMock<mock_render_surface_capabilities> surface_capabilities_;
+    munin::titled_frame frame_{"title"};
+};
+
+using a_titled_frame = a_titled_frame_with_no_unicode_support;
+
+class a_titled_frame_with_unicode_support : public testing::Test
+{
+protected :
+    void SetUp() override
+    {
+        ON_CALL(surface_capabilities_, supports_unicode())
+            .WillByDefault(Return(true));
+    }
+
+    NiceMock<mock_render_surface_capabilities> surface_capabilities_;
+    munin::titled_frame frame_{"title"};
+};
+
+TEST_F(a_titled_frame, is_a_component)
+{
+    std::shared_ptr<munin::component> comp = munin::make_titled_frame("title");
+}
+
+/*
+TEST_F(a_titled_frame_with_no_unicode_support, draws_a_border)
+{
+    frame_.set_size({4, 4});
+    
+    terminalpp::canvas canvas({4, 4});
+    munin::render_surface surface{canvas, surface_capabilities_};
+    frame_.draw(surface, {{}, {4, 4}});
+
+    ASSERT_EQ(top_left_corner,     canvas[0][0]);
+    ASSERT_EQ(horizontal_beam,     canvas[1][0]);
+    ASSERT_EQ(horizontal_beam,     canvas[2][0]);
+    ASSERT_EQ(top_right_corner,    canvas[3][0]);
+    ASSERT_EQ(vertical_beam,       canvas[0][1]);
+    ASSERT_EQ(vertical_beam,       canvas[3][1]);
+    ASSERT_EQ(vertical_beam,       canvas[0][2]);
+    ASSERT_EQ(vertical_beam,       canvas[3][2]);
+    ASSERT_EQ(bottom_left_corner,  canvas[0][3]);
+    ASSERT_EQ(horizontal_beam,     canvas[1][3]);
+    ASSERT_EQ(horizontal_beam,     canvas[2][3]);
+    ASSERT_EQ(bottom_right_corner, canvas[3][3]);
+}
+
+TEST_F(a_titled_frame_with_unicode_support, draws_a_border_with_box_drawing_glyphs)
+{
+    frame_.set_size({4, 4});
+    
+    terminalpp::canvas canvas({4, 4});
+    munin::render_surface surface{canvas, surface_capabilities_};
+    frame_.draw(surface, {{}, {4, 4}});
+
+    ASSERT_EQ(unicode_top_left_corner,     canvas[0][0]);
+    ASSERT_EQ(unicode_horizontal_beam,     canvas[1][0]);
+    ASSERT_EQ(unicode_horizontal_beam,     canvas[2][0]);
+    ASSERT_EQ(unicode_top_right_corner,    canvas[3][0]);
+    ASSERT_EQ(unicode_vertical_beam,       canvas[0][1]);
+    ASSERT_EQ(unicode_vertical_beam,       canvas[0][2]);
+    ASSERT_EQ(unicode_vertical_beam,       canvas[3][2]);
+    ASSERT_EQ(unicode_vertical_beam,       canvas[3][1]);
+    ASSERT_EQ(unicode_bottom_left_corner,  canvas[0][3]);
+    ASSERT_EQ(unicode_horizontal_beam,     canvas[1][3]);
+    ASSERT_EQ(unicode_horizontal_beam,     canvas[2][3]);
+    ASSERT_EQ(unicode_bottom_right_corner, canvas[3][3]);
+}
+
+TEST_F(a_titled_frame, can_be_displayed_with_a_custom_lowlight)
+{
+    static auto const lowlight_attribute = terminalpp::attribute(
+        terminalpp::ansi::graphics::colour::green,
+        terminalpp::colour(),
+        terminalpp::ansi::graphics::intensity::bold);
+        
+    frame_.set_size({4, 4});
+    frame_.set_lowlight_attribute(lowlight_attribute);
+    
+    terminalpp::canvas canvas({4, 4});
+    munin::render_surface surface{canvas, surface_capabilities_};
+    frame_.draw(surface, {{}, {4, 4}});
+
+    ASSERT_EQ(terminalpp::element('+', lowlight_attribute), canvas[0][0]);
+    ASSERT_EQ(terminalpp::element('-', lowlight_attribute), canvas[1][0]);
+    ASSERT_EQ(terminalpp::element('-', lowlight_attribute), canvas[2][0]);
+    ASSERT_EQ(terminalpp::element('+', lowlight_attribute), canvas[3][0]);
+    ASSERT_EQ(terminalpp::element('|', lowlight_attribute), canvas[0][1]);
+    ASSERT_EQ(terminalpp::element('|', lowlight_attribute), canvas[3][1]);
+    ASSERT_EQ(terminalpp::element('|', lowlight_attribute), canvas[0][2]);
+    ASSERT_EQ(terminalpp::element('|', lowlight_attribute), canvas[3][2]);
+    ASSERT_EQ(terminalpp::element('+', lowlight_attribute), canvas[0][3]);
+    ASSERT_EQ(terminalpp::element('-', lowlight_attribute), canvas[1][3]);
+    ASSERT_EQ(terminalpp::element('-', lowlight_attribute), canvas[2][3]);
+    ASSERT_EQ(terminalpp::element('+', lowlight_attribute), canvas[3][3]);
+}
+
+class a_titled_frame_with_an_associated_unfocussed_component : public a_titled_frame_with_no_unicode_support
+{
+protected :
+    void SetUp() override
+    {
+        frame_.highlight_on_focus(comp_);
+        ON_CALL(*comp_, do_has_focus())
+            .WillByDefault(Return(false));
+    }
+    
+    std::shared_ptr<mock_component> comp_ = make_mock_component();
+};
+
+class a_titled_frame_with_an_associated_focussed_component : public a_titled_frame_with_no_unicode_support
+{
+protected :
+    void SetUp() override
+    {
+        frame_.highlight_on_focus(comp_);
+        ON_CALL(*comp_, do_has_focus())
+            .WillByDefault(Return(true));
+    }
+    
+    std::shared_ptr<mock_component> comp_ = make_mock_component();
+};
+
+TEST_F(a_titled_frame_with_an_associated_unfocussed_component, draws_a_lowlighted_border)
+{
+    frame_.set_size({4, 4});
+
+    terminalpp::canvas canvas({4, 4});
+    munin::render_surface surface{canvas, surface_capabilities_};
+    frame_.draw(surface, {{}, {4, 4}});
+
+    ASSERT_EQ(top_left_corner,     canvas[0][0]);
+    ASSERT_EQ(horizontal_beam,     canvas[1][0]);
+    ASSERT_EQ(horizontal_beam,     canvas[2][0]);
+    ASSERT_EQ(top_right_corner,    canvas[3][0]);
+    ASSERT_EQ(vertical_beam,       canvas[0][1]);
+    ASSERT_EQ(vertical_beam,       canvas[3][1]);
+    ASSERT_EQ(vertical_beam,       canvas[0][2]);
+    ASSERT_EQ(vertical_beam,       canvas[3][2]);
+    ASSERT_EQ(bottom_left_corner,  canvas[0][3]);
+    ASSERT_EQ(horizontal_beam,     canvas[1][3]);
+    ASSERT_EQ(horizontal_beam,     canvas[2][3]);
+    ASSERT_EQ(bottom_right_corner, canvas[3][3]);
+}
+
+TEST_F(a_titled_frame_with_an_associated_unfocussed_component, when_focussed_draws_a_highlighted_border)
+{
+    frame_.set_size({4, 4});
+
+    ON_CALL(*comp_, do_has_focus())
+        .WillByDefault(Return(true));
+    comp_->on_focus_set();
+
+    terminalpp::canvas canvas({4, 4});
+    munin::render_surface surface{canvas, surface_capabilities_};
+    frame_.draw(surface, {{}, {4, 4}});
+
+    ASSERT_EQ(terminalpp::element(top_left_corner,     highlight_attribute), canvas[0][0]);
+    ASSERT_EQ(terminalpp::element(horizontal_beam,     highlight_attribute), canvas[1][0]);
+    ASSERT_EQ(terminalpp::element(horizontal_beam,     highlight_attribute), canvas[2][0]);
+    ASSERT_EQ(terminalpp::element(top_right_corner,    highlight_attribute), canvas[3][0]);
+    ASSERT_EQ(terminalpp::element(vertical_beam,       highlight_attribute), canvas[3][2]);
+    ASSERT_EQ(terminalpp::element(vertical_beam,       highlight_attribute), canvas[0][1]);
+    ASSERT_EQ(terminalpp::element(vertical_beam,       highlight_attribute), canvas[3][1]);
+    ASSERT_EQ(terminalpp::element(vertical_beam,       highlight_attribute), canvas[0][2]);
+    ASSERT_EQ(terminalpp::element(bottom_left_corner,  highlight_attribute), canvas[0][3]);
+    ASSERT_EQ(terminalpp::element(horizontal_beam,     highlight_attribute), canvas[1][3]);
+    ASSERT_EQ(terminalpp::element(horizontal_beam,     highlight_attribute), canvas[2][3]);
+    ASSERT_EQ(terminalpp::element(bottom_right_corner, highlight_attribute), canvas[3][3]);
+}
+
+TEST_F(a_titled_frame_with_an_associated_focussed_component, when_unfocussed_draws_a_lowlit_border)
+{
+    frame_.set_size({4, 4});
+
+    ON_CALL(*comp_, do_has_focus())
+        .WillByDefault(Return(false));
+    comp_->on_focus_lost();
+
+    terminalpp::canvas canvas({4, 4});
+    munin::render_surface surface{canvas, surface_capabilities_};
+    frame_.draw(surface, {{}, {4, 4}});
+
+    ASSERT_EQ(top_left_corner,     canvas[0][0]);
+    ASSERT_EQ(horizontal_beam,     canvas[1][0]);
+    ASSERT_EQ(horizontal_beam,     canvas[2][0]);
+    ASSERT_EQ(top_right_corner,    canvas[3][0]);
+    ASSERT_EQ(vertical_beam,       canvas[0][1]);
+    ASSERT_EQ(vertical_beam,       canvas[3][1]);
+    ASSERT_EQ(vertical_beam,       canvas[0][2]);
+    ASSERT_EQ(vertical_beam,       canvas[3][2]);
+    ASSERT_EQ(bottom_left_corner,  canvas[0][3]);
+    ASSERT_EQ(horizontal_beam,     canvas[1][3]);
+    ASSERT_EQ(horizontal_beam,     canvas[2][3]);
+    ASSERT_EQ(bottom_right_corner, canvas[3][3]);
+}
+
+TEST_F(a_titled_frame_with_an_associated_focussed_component, can_have_a_custom_highlight)
+{
+    terminalpp::attribute custom_highlight = {
+        terminalpp::ansi::graphics::colour::green,
+        terminalpp::ansi::graphics::colour::magenta
+    };
+    
+    frame_.set_highlight_attribute(custom_highlight);
+    frame_.set_size({4, 4});
+
+    ON_CALL(*comp_, do_has_focus())
+        .WillByDefault(Return(true));
+    comp_->on_focus_set();
+
+    terminalpp::canvas canvas({4, 4});
+    munin::render_surface surface{canvas, surface_capabilities_};
+    frame_.draw(surface, {{}, {4, 4}});
+
+    ASSERT_EQ(terminalpp::element(top_left_corner,     custom_highlight), canvas[0][0]);
+    ASSERT_EQ(terminalpp::element(horizontal_beam,     custom_highlight), canvas[1][0]);
+    ASSERT_EQ(terminalpp::element(horizontal_beam,     custom_highlight), canvas[2][0]);
+    ASSERT_EQ(terminalpp::element(top_right_corner,    custom_highlight), canvas[3][0]);
+    ASSERT_EQ(terminalpp::element(vertical_beam,       custom_highlight), canvas[3][2]);
+    ASSERT_EQ(terminalpp::element(vertical_beam,       custom_highlight), canvas[0][1]);
+    ASSERT_EQ(terminalpp::element(vertical_beam,       custom_highlight), canvas[3][1]);
+    ASSERT_EQ(terminalpp::element(vertical_beam,       custom_highlight), canvas[0][2]);
+    ASSERT_EQ(terminalpp::element(bottom_left_corner,  custom_highlight), canvas[0][3]);
+    ASSERT_EQ(terminalpp::element(horizontal_beam,     custom_highlight), canvas[1][3]);
+    ASSERT_EQ(terminalpp::element(horizontal_beam,     custom_highlight), canvas[2][3]);
+    ASSERT_EQ(terminalpp::element(bottom_right_corner, custom_highlight), canvas[3][3]);
+}
+*/
