381d142d69ced0a2571e9079b017790c821782ad
KazDragon <matthew.chaplain@gmail.com>
2018-08-13 15:35:46 +0000

    Refactored solid frame to use adaptive fills.

--- src/solid_frame.cpp
+++ src/solid_frame.cpp
@@ -1,90 +1,10 @@
 #include "munin/solid_frame.hpp"
-#include "munin/detail/unicode_glyphs.hpp"
+#include "munin/detail/adaptive_fill.hpp"
 #include "munin/compass_layout.hpp"
-#include "munin/filled_box.hpp"
 #include "munin/render_surface.hpp"
 #include "munin/view.hpp"
 
 namespace munin {
-namespace {
-
-constexpr terminalpp::glyph const default_corner_element          = '+';
-constexpr terminalpp::glyph const default_horizontal_beam_element = '-';
-constexpr terminalpp::glyph const default_vertical_beam_element   = '|';
-    
-// ==========================================================================
-// TOP_LEFT_CORNER_GLYPH
-// ==========================================================================
-terminalpp::element select_top_left_corner_element(
-    render_surface const &surface,
-    terminalpp::attribute const &attr)
-{
-    return {surface.supports_unicode()
-         ? detail::single_lined_rounded_top_left_corner
-         : default_corner_element, attr};
-}
-
-// ==========================================================================
-// TOP_RIGHT_CORNER_GLYPH
-// ==========================================================================
-terminalpp::element select_top_right_corner_element(
-    render_surface const &surface,
-    terminalpp::attribute const &attr)
-{
-    return {surface.supports_unicode()
-         ? detail::single_lined_rounded_top_right_corner
-         : default_corner_element, attr};
-}
-
-// ==========================================================================
-// BOTTOM_LEFT_CORNER_GLYPH
-// ==========================================================================
-terminalpp::element select_bottom_left_corner_element(
-    render_surface const &surface,
-    terminalpp::attribute const &attr)
-{
-    return {surface.supports_unicode()
-         ? detail::single_lined_rounded_bottom_left_corner
-         : default_corner_element, attr};
-}
-
-// ==========================================================================
-// BOTTOM_RIGHT_CORNER_GLYPH
-// ==========================================================================
-terminalpp::element select_bottom_right_corner_element(
-    render_surface const &surface,
-    terminalpp::attribute const &attr)
-{
-    return {surface.supports_unicode()
-         ? detail::single_lined_rounded_bottom_right_corner
-         : default_corner_element, attr};
-}
-
-// ==========================================================================
-// HORIZONTAL_BEAM_GLYPH
-// ==========================================================================
-terminalpp::element select_horizontal_beam_element(
-    render_surface const &surface,
-    terminalpp::attribute const &attr)
-{
-    return {surface.supports_unicode()
-         ? detail::single_lined_horizontal_beam
-         : default_horizontal_beam_element, attr};
-}
-
-// ==========================================================================
-// VERTICAL_BEAM_GLYPH
-// ==========================================================================
-terminalpp::element select_vertical_beam_element(
-    render_surface const &surface,
-    terminalpp::attribute const &attr)
-{
-    return {surface.supports_unicode()
-         ? detail::single_lined_vertical_beam
-         : default_vertical_beam_element, attr};
-}
-
-}
 
 struct solid_frame::impl
 {
@@ -94,54 +14,6 @@ struct solid_frame::impl
         terminalpp::colour(),
         terminalpp::ansi::graphics::intensity::bold};
     terminalpp::attribute *current_attribute = &lowlight_attribute;
-
-    std::shared_ptr<munin::filled_box> top_left = munin::make_fill(
-        [this](render_surface &surface)
-        {
-            return select_top_left_corner_element(surface, *current_attribute);
-        });
-            
-    std::shared_ptr<munin::filled_box> top_centre = munin::make_fill(
-        [this](render_surface &surface)
-        {
-            return select_horizontal_beam_element(surface, *current_attribute);
-        });
-        
-    std::shared_ptr<munin::filled_box> top_right = munin::make_fill(
-        [this](render_surface &surface)
-        {
-            return select_top_right_corner_element(surface, *current_attribute);
-        });
-        
-    std::shared_ptr<munin::filled_box> centre_left = munin::make_fill(
-        [this](render_surface &surface)
-        {
-            return select_vertical_beam_element(surface, *current_attribute);
-        });
-        
-    std::shared_ptr<munin::filled_box> centre_right = munin::make_fill(
-        [this](render_surface &surface)
-        {
-            return select_vertical_beam_element(surface, *current_attribute);
-        });
-        
-    std::shared_ptr<munin::filled_box> bottom_left = munin::make_fill(
-        [this](render_surface &surface)
-        {
-            return select_bottom_left_corner_element(surface, *current_attribute);
-        });
-        
-    std::shared_ptr<munin::filled_box> bottom_centre = munin::make_fill(
-        [this](render_surface &surface)
-        {
-            return select_horizontal_beam_element(surface, *current_attribute);
-        });
-        
-    std::shared_ptr<munin::filled_box> bottom_right = munin::make_fill(
-        [this](render_surface &surface)
-        {
-            return select_bottom_right_corner_element(surface, *current_attribute);
-        });
 };
 
 // ==========================================================================
@@ -150,20 +22,22 @@ struct solid_frame::impl
 solid_frame::solid_frame()
   : pimpl_(std::make_shared<impl>())
 {
+    auto &attr = pimpl_->current_attribute;
+    
     auto north_beam = view(
         make_compass_layout(),
-        pimpl_->top_left,   compass_layout::heading::west,
-        pimpl_->top_centre, compass_layout::heading::centre,
-        pimpl_->top_right,  compass_layout::heading::east);
+        detail::make_top_left_corner_fill(attr), compass_layout::heading::west,
+        detail::make_horizontal_beam_fill(attr), compass_layout::heading::centre,
+        detail::make_top_right_corner_fill(attr),  compass_layout::heading::east);
     
     auto south_beam = view(
         make_compass_layout(),
-        pimpl_->bottom_left, compass_layout::heading::west,
-        pimpl_->bottom_centre, compass_layout::heading::centre,
-        pimpl_->bottom_right, compass_layout::heading::east);
+        detail::make_bottom_left_corner_fill(attr), compass_layout::heading::west,
+        detail::make_horizontal_beam_fill(attr), compass_layout::heading::centre,
+        detail::make_bottom_right_corner_fill(attr), compass_layout::heading::east);
         
-    auto west_beam = pimpl_->centre_left;
-    auto east_beam = pimpl_->centre_right;
+    auto west_beam = detail::make_vertical_beam_fill(attr);
+    auto east_beam = detail::make_vertical_beam_fill(attr);
     
     set_layout(make_compass_layout());
     
