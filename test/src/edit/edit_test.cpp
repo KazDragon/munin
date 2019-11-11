b43d0951b6719f6b725da6486f41e79c49ac47f2
KazDragon <matthew.chaplain@gmail.com>
2019-11-11 07:17:31 +0000

    Test for inserting printable characters now checks various characters.

--- test/src/edit/edit_test.cpp
+++ test/src/edit/edit_test.cpp
@@ -7,25 +7,30 @@
 #include <gtest/gtest.h>
 
 using namespace terminalpp::literals;
+using testing::ValuesIn;
+
+namespace {
+    
+void fill_canvas(
+    terminalpp::canvas &canvas, 
+    terminalpp::element const &fill = ' ')
+{
+    terminalpp::for_each_in_region(
+        canvas,
+        {{}, canvas.size()},
+        [fill](terminalpp::element &elem,
+               terminalpp::coordinate_type column,
+               terminalpp::coordinate_type row)
+        {
+            elem = fill;
+        });
+}
+
+}
 
 class a_new_edit : public testing::Test
 {
 protected:
-    void fill_canvas(
-        terminalpp::canvas &canvas, 
-        terminalpp::element const &fill = ' ')
-    {
-        terminalpp::for_each_in_region(
-            canvas,
-            {{}, canvas.size()},
-            [fill](terminalpp::element &elem,
-                   terminalpp::coordinate_type column,
-                   terminalpp::coordinate_type row)
-            {
-                elem = fill;
-            });
-    }
-
     std::shared_ptr<munin::edit> edit_ = munin::make_edit();
 };
 
@@ -158,31 +163,68 @@ TEST_F(a_new_edit, draws_inserted_text_cursor_at_end)
     ASSERT_EQ(terminalpp::element{'x'}, cvs[3][2]);
 }
 
-TEST_F(a_new_edit, inserts_a_character_when_a_keypress_is_received)
+using printable_character_test_data = std::tuple<
+    terminalpp::vk,      // character code
+    terminalpp::element  // representation
+>;
+
+class printable_character_test 
+  : public testing::TestWithParam<printable_character_test_data>
 {
-    terminalpp::canvas cvs{{4, 3}};
-    fill_canvas(cvs, 'x');
+public:
+    printable_character_test()
+      : cvs_({4, 3}),
+        surface_(cvs_)
+    {
+        edit_.set_position({1, 1});
+        edit_.set_size({2, 1});
     
-    edit_->set_position({1, 1});
-    edit_->set_size({2, 1});
-
-    terminalpp::virtual_key keypress{terminalpp::vk::uppercase_t};
-    edit_->event(keypress);
+        fill_canvas(cvs_, 'x');
+        
+        surface_.offset_by({1, 1});
+    }
     
-    munin::render_surface surface{cvs};
-    surface.offset_by({1, 1});
-    edit_->draw(surface, {{}, edit_->get_size()});
+protected:
+    munin::edit          edit_;
+    terminalpp::canvas   cvs_;
+    munin::render_surface surface_;
+};
 
-    ASSERT_EQ(terminalpp::element{'x'}, cvs[0][0]);
-    ASSERT_EQ(terminalpp::element{'x'}, cvs[1][0]);
-    ASSERT_EQ(terminalpp::element{'x'}, cvs[2][0]);
-    ASSERT_EQ(terminalpp::element{'x'}, cvs[3][0]);
-    ASSERT_EQ(terminalpp::element{'x'}, cvs[0][1]);
-    ASSERT_EQ(terminalpp::element{'T'}, cvs[1][1]);
-    ASSERT_EQ(terminalpp::element{' '}, cvs[2][1]);
-    ASSERT_EQ(terminalpp::element{'x'}, cvs[3][1]);
-    ASSERT_EQ(terminalpp::element{'x'}, cvs[0][2]);
-    ASSERT_EQ(terminalpp::element{'x'}, cvs[1][2]);
-    ASSERT_EQ(terminalpp::element{'x'}, cvs[2][2]);
-    ASSERT_EQ(terminalpp::element{'x'}, cvs[3][2]);
+TEST_P(printable_character_test, a_character_is_inserted_when_a_printable_character_keypress_is_received)
+{
+    using std::get;
+    
+    auto const params = GetParam();
+    auto const keypress = get<0>(params);
+    auto const expected = get<1>(params);
+    
+    edit_.event(terminalpp::virtual_key{keypress});
+    
+    edit_.draw(surface_, {{}, edit_.get_size()});
+
+    ASSERT_EQ(terminalpp::element{'x'}, cvs_[0][0]);
+    ASSERT_EQ(terminalpp::element{'x'}, cvs_[1][0]);
+    ASSERT_EQ(terminalpp::element{'x'}, cvs_[2][0]);
+    ASSERT_EQ(terminalpp::element{'x'}, cvs_[3][0]);
+    ASSERT_EQ(terminalpp::element{'x'}, cvs_[0][1]);
+    ASSERT_EQ(expected,                 cvs_[1][1]);
+    ASSERT_EQ(terminalpp::element{' '}, cvs_[2][1]);
+    ASSERT_EQ(terminalpp::element{'x'}, cvs_[3][1]);
+    ASSERT_EQ(terminalpp::element{'x'}, cvs_[0][2]);
+    ASSERT_EQ(terminalpp::element{'x'}, cvs_[1][2]);
+    ASSERT_EQ(terminalpp::element{'x'}, cvs_[2][2]);
+    ASSERT_EQ(terminalpp::element{'x'}, cvs_[3][2]);
 }
+
+INSTANTIATE_TEST_CASE_P(
+    printable_characters,
+    printable_character_test,
+    ValuesIn
+    ({
+        printable_character_test_data{ terminalpp::vk::uppercase_t, 'T' },
+        printable_character_test_data{ terminalpp::vk::lowercase_t, 't' },
+        printable_character_test_data{ terminalpp::vk::uppercase_z, 'Z' },
+        printable_character_test_data{ terminalpp::vk::space,       ' ' },
+        printable_character_test_data{ terminalpp::vk::dollar,      '$' },
+    })
+);
