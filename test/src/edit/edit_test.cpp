2ea74710619ee4562f0f7aba44a1340f276d7216
KazDragon <matthew.chaplain@gmail.com>
2019-11-15 07:50:44 +0000

    Refactored fill_canvas into its own file.

--- test/src/edit/edit_test.cpp
+++ test/src/edit/edit_test.cpp
@@ -1,6 +1,6 @@
+#include "fill_canvas.hpp"
 #include <munin/edit.hpp>
 #include <munin/render_surface.hpp>
-#include <terminalpp/algorithm/for_each_in_region.hpp>
 #include <terminalpp/canvas.hpp>
 #include <terminalpp/element.hpp>
 #include <terminalpp/virtual_key.hpp>
@@ -8,25 +8,6 @@
 
 using namespace terminalpp::literals;
 using testing::ValuesIn;
-
-namespace {
-    
-void fill_canvas(
-    terminalpp::canvas &canvas, 
-    terminalpp::element const &fill = ' ')
-{
-    terminalpp::for_each_in_region(
-        canvas,
-        {{}, canvas.size()},
-        [fill](terminalpp::element &elem,
-               terminalpp::coordinate_type column,
-               terminalpp::coordinate_type row)
-        {
-            elem = fill;
-        });
-}
-
-}
 
 class a_new_edit : public testing::Test
 {
