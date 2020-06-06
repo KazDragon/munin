35b41b20bdea758be31e96617226b6f4dbe37ee8
KazDragon <matthew.chaplain@gmail.com>
2020-06-06 11:15:58 +0200

    Update to version 0.3.4

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class MuninConan(ConanFile):
     name = "munin"
-    version = "0.3.3"
+    version = "0.3.4"
     license = "MIT"
     author = "KazDragon"
     url = "https://github.com/KazDragon/munin"
