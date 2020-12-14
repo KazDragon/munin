32a3a7b48cd1600f6560b0192b489dec1ae1fb64
Matthew Chaplain <matthew.chaplain@gmail.com>
2020-12-14 20:04:50 +0100

    Removed temporary main file from tests

--- test/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-// This is a (hopefully) temporary hack for MSVC, which for some reason does not automatically
-// detect or run tests if GoogleMock is being used.
-int main(int argc, char **argv)
-{
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
