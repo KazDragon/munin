fbc69b2d885afdbc4a4f67c24e7010ee0f66d56b
KazDragon <matthew.chaplain@gmail.com>
2021-03-09 12:20:43 +0100

    Munin now requires Terminal++ 2.0.1.
    This contains a fix for the libfmt dependency.

--- .ci/fetch_system_dependencies.sh
+++ .ci/fetch_system_dependencies.sh
@@ -37,9 +37,9 @@ fi
 
 # Install Terminal++ dependency
 if [ ! -f "$EXTERNAL_ROOT/include/terminalpp/version/hpp" ]; then
-    wget https://github.com/KazDragon/terminalpp/archive/v2.0.0.tar.gz;
-    tar -xzf v2.0.0.tar.gz;
-    cd terminalpp-2.0.0;
+    wget https://github.com/KazDragon/terminalpp/archive/v2.0.1.tar.gz;
+    tar -xzf v2.0.1.tar.gz;
+    cd terminalpp-2.0.1;
     cmake -DCMAKE_INSTALL_PREFIX="$EXTERNAL_ROOT" -DTERMINALPP_WITH_TESTS=False .;
     make -j2 && make install;
     cd ..;
