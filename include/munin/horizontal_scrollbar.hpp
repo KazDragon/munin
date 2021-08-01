91eca7cb7396faa1798426daf8a344eb1aeaeb48
Matthew Chaplain <matthew.chaplain@gmail.com>
2021-08-01 15:02:32 +0200

    Added skeleton of a horizontal scrollbar.

---
+++ include/munin/horizontal_scrollbar.hpp
@@ -0,0 +1,32 @@
+#include "munin/basic_component.hpp"
+
+namespace munin {
+
+class MUNIN_EXPORT horizontal_scrollbar : public basic_component
+{
+private:
+    //* =====================================================================
+    /// \brief Called by get_preferred_size().  Derived classes must override
+    /// this function in order to get the size of the component in a custom
+    /// manner.
+    //* =====================================================================
+    terminalpp::extent do_get_preferred_size() const override;
+
+    //* =====================================================================
+    /// \brief Called by draw().  Derived classes must override this function
+    /// in order to draw onto the passed context.  A component must only draw
+    /// the part of itself specified by the region.
+    ///
+    /// \param surface the surface on which the component should draw itself.
+    /// \param region the region relative to this component's origin that
+    /// should be drawn.
+    //* =====================================================================
+    void do_draw(
+        render_surface &surface,
+        terminalpp::rectangle const &region) const override;
+};
+
+MUNIN_EXPORT
+std::shared_ptr<horizontal_scrollbar> make_horizontal_scrollbar();
+
+}
