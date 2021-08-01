652599b4bd8b26fc91a9b990faa92e96db88aa89
Matthew Chaplain <matthew.chaplain@gmail.com>
2021-08-01 15:20:27 +0200

    Horizontal scrollbar appears as a solid frame when there is no slider.

--- src/horizontal_scrollbar.cpp
+++ src/horizontal_scrollbar.cpp
@@ -1,4 +1,7 @@
 #include "munin/horizontal_scrollbar.hpp"
+#include "munin/detail/unicode_glyphs.hpp"
+#include "munin/render_surface.hpp"
+#include <terminalpp/algorithm/for_each_in_region.hpp>
 #include <memory>
 
 namespace munin {
@@ -12,6 +15,15 @@ void horizontal_scrollbar::do_draw(
     render_surface &surface,
     terminalpp::rectangle const &region) const
 {
+    terminalpp::for_each_in_region(
+        surface,
+        region,
+        [this](terminalpp::element &elem, 
+               terminalpp::coordinate_type column, 
+               terminalpp::coordinate_type row)
+        {
+            elem = munin::detail::single_lined_horizontal_beam;
+        });
 }
 
 std::shared_ptr<horizontal_scrollbar> make_horizontal_scrollbar()
