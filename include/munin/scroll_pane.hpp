ff61a9c9012ff8ba8e988bf0e672a10e8ff9a3da
Matthew Chaplain <matthew.chaplain@gmail.com>
2021-08-05 16:42:07 +0200

    Editorial changes.

--- include/munin/scroll_pane.hpp
+++ include/munin/scroll_pane.hpp
@@ -4,13 +4,34 @@
 
 namespace munin {
 
+//* =========================================================================
+/// \brief A component that wraps another component in a frame,
+/// and implements scroll bars that track the visible section of the 
+/// component.
+//* =========================================================================
 class MUNIN_EXPORT scroll_pane : public framed_component
 {
 public:
+    //* =====================================================================
+    /// \brief Constructor
+    //* =====================================================================
     scroll_pane(std::shared_ptr<component> const &inner_component);
+
+    //* =====================================================================
+    /// \brief Destructor
+    //* =====================================================================
     ~scroll_pane();
 
+    //* =====================================================================
+    /// \brief Sets the attribute of the elements when the inner component
+    /// has focus.
+    //* =====================================================================
     void set_highlight_attribute(terminalpp::attribute const &attr);
+
+    //* =====================================================================
+    /// \brief Sets the attribute of the elements when the inner component
+    /// does not have focus.
+    //* =====================================================================
     void set_lowlight_attribute(terminalpp::attribute const &attr);
 
 private:
@@ -22,7 +43,6 @@ private:
     struct impl;
     std::unique_ptr<impl> pimpl_;
 };
-
 
 MUNIN_EXPORT
 std::shared_ptr<scroll_pane> make_scroll_pane(std::shared_ptr<component> const &inner_component);
