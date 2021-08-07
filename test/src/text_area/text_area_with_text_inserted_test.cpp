2f8ad71cfaa7b33e134eafb302a937bdf52187e2
Matthew Chaplain <matthew.chaplain@gmail.com>
2021-08-07 22:18:42 +0200

    Refactored text_area with learnings from edit.

--- test/src/text_area/text_area_with_text_inserted_test.cpp
+++ test/src/text_area/text_area_with_text_inserted_test.cpp
@@ -132,3 +132,22 @@ TEST_F(a_text_area_with_text_inserted, lays_out_its_text_when_the_size_changes)
     verify_oob_is_untouched();
 }
 
+TEST_F(a_text_area_with_text_inserted, announces_a_change_in_preferred_size_if_its_size_is_changed_such_that_there_are_now_more_lines)
+{
+    // for example, if I change the size of "ab" to 1x1 such that the b would
+    // now bleed into the next line, the text area should request the new
+    // preferred height.
+    text_area_.set_size({2, 1});
+
+    auto preferred_size = terminalpp::extent{};
+    text_area_.on_preferred_size_changed.connect(
+        [&]
+        {
+            preferred_size = text_area_.get_preferred_size();
+        });
+
+    text_area_.set_size({1, 1});
+
+    auto const expected_preferred_size = terminalpp::extent{1, 3};
+    ASSERT_EQ(expected_preferred_size, preferred_size);
+}
