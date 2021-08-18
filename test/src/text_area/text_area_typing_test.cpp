70d3fd907195dfcfadb14e0dac1a9f871e1f74d4
Matthew Chaplain <matthew.chaplain@gmail.com>
2021-08-18 09:17:36 +0200

    Refactored to parametric tests.

--- test/src/text_area/text_area_typing_test.cpp
+++ test/src/text_area/text_area_typing_test.cpp
@@ -4,56 +4,68 @@
 #include <terminalpp/virtual_key.hpp>
 
 using namespace terminalpp::literals;
+using testing::ValuesIn;
 
-TEST_F(a_text_area, appends_typed_keys_at_the_cursor)
+using keypress_test_data = std::tuple<
+    terminalpp::string,      // inserted text
+    terminalpp::point,       // cursor position
+    terminalpp::virtual_key, // keypress
+    terminalpp::string,      // expected text
+    terminalpp::point        // expected cursor position
+>;
+
+namespace {
+
+class a_text_area_receiving_keypresses
+  : public a_text_area_base,
+    public testing::TestWithParam<keypress_test_data>
 {
-    {
-        auto const keypress_a = terminalpp::virtual_key {
-            terminalpp::vk::lowercase_a,
-            terminalpp::vk_modifier::none,
-            1
-        };
+};
 
-        text_area_.event(keypress_a);
+auto const keypress_a = terminalpp::virtual_key {
+    terminalpp::vk::lowercase_a,
+    terminalpp::vk_modifier::none,
+    1
+};
 
-        auto const expected_text = terminalpp::string("a");
+auto const keypress_b = terminalpp::virtual_key {
+    terminalpp::vk::uppercase_b,
+    terminalpp::vk_modifier::none,
+    1,
+};
 
-        ASSERT_EQ(expected_text, text_area_.get_text());
-    }
+auto const keypress_c = terminalpp::virtual_key {
+    terminalpp::vk::uppercase_c,
+    terminalpp::vk_modifier::none,
+    1,
+};
 
-    {
-        auto const keypress_b = terminalpp::virtual_key {
-            terminalpp::vk::uppercase_b,
-            terminalpp::vk_modifier::none,
-            1,
-        };
+auto const keypress_bs = terminalpp::virtual_key {
+    terminalpp::vk::bs,
+    terminalpp::vk_modifier::none,
+    1
+};
 
-        text_area_.event(keypress_b);
+auto const keypress_del = terminalpp::virtual_key {
+    terminalpp::vk::del,
+    terminalpp::vk_modifier::none,
+    1
+};
 
-        auto const expected_text = terminalpp::string("aB");
-
-        ASSERT_EQ(expected_text, text_area_.get_text());
-    }
-
-    {
-        auto const keypress_c = terminalpp::virtual_key {
-            terminalpp::vk::uppercase_c,
-            terminalpp::vk_modifier::none,
-            1,
-        };
-
-        text_area_.set_caret_position(0);
-        text_area_.event(keypress_c);
-
-        auto const expected_text = terminalpp::string("CaB");
-
-        ASSERT_EQ(expected_text, text_area_.get_text());
-    }
 }
 
-TEST_F(a_text_area, requests_a_redraw_when_text_is_added_via_keypress)
+TEST_P(a_text_area_receiving_keypresses, responds_in_the_specified_manner)
 {
-    terminalpp::canvas cvs({3, 3});
+    using std::get;
+
+    auto const &params = GetParam();
+    auto const &inserted_text = get<0>(params);
+    auto const &cursor_position = get<1>(params);
+    auto const &keypress = get<2>(params);
+    auto const &expected_text = get<3>(params);
+    auto const &expected_cursor_position = get<4>(params);
+
+    terminalpp::canvas cvs({10, 1});
     munin::render_surface surface{cvs};
 
     text_area_.on_redraw.connect(
@@ -65,120 +77,50 @@ TEST_F(a_text_area, requests_a_redraw_when_text_is_added_via_keypress)
             }
         });
 
-    text_area_.set_size({3, 3});
+    text_area_.set_size({10, 1});
+    text_area_.insert_text(inserted_text);
+    text_area_.set_cursor_position(cursor_position);
 
-    auto const keypress_d = terminalpp::virtual_key {
-        terminalpp::vk::lowercase_d,
-        terminalpp::vk_modifier::none,
-        1
-    };
-
-    text_area_.event(keypress_d);
-
-    ASSERT_EQ(terminalpp::element{'d'}, cvs[0][0]);
-}
-
-TEST_F(a_text_area, updates_the_cursor_position_when_text_is_added_via_keypress)
-{
-    auto cursor_position = terminalpp::point{};
+    auto resultant_cursor_position = cursor_position;
     text_area_.on_cursor_position_changed.connect(
-        [&cursor_position, this]()
+        [&resultant_cursor_position, this]
         {
-            cursor_position = text_area_.get_cursor_position();
+            resultant_cursor_position = text_area_.get_cursor_position();
         });
+        
+    text_area_.event(keypress);
+    
+    auto const &text = text_area_.get_text();
+    EXPECT_EQ(expected_text, text);
+    EXPECT_EQ(expected_cursor_position, resultant_cursor_position);
 
-    text_area_.set_size({3, 3});
-
-    auto const keypress_e = terminalpp::virtual_key {
-        terminalpp::vk::uppercase_e,
-        terminalpp::vk_modifier::none,
-        1
-    };
-
-    text_area_.event(keypress_e);
-
-    auto const expected_cursor_position = terminalpp::point{1, 0};
-    ASSERT_EQ(expected_cursor_position, cursor_position);
+    for (terminalpp::coordinate_type col = 0; col < cvs.size().width_; ++col)
+    {
+        if (col < text.size())
+        {
+            EXPECT_EQ(text[col], cvs[col][0]) 
+                << "at column " << col;
+        }
+        else
+        {
+            EXPECT_EQ(terminalpp::element{' '}, cvs[col][0]) 
+                << "at column " << col;
+        }
+    }
 }
 
-TEST_F(a_text_area, deletes_text_before_the_cursor_when_backspace_is_pressed)
+static keypress_test_data const keypress_data[] = 
 {
-    auto cvs = terminalpp::canvas({10, 3});
-    text_area_.set_size({10, 3});
+    { ""_ts,     {0, 0}, keypress_a,   "a"_ts,   {1, 0} },
+    { "a"_ts,    {1, 0}, keypress_b,   "aB"_ts,  {2, 0} },
+    { "aB"_ts,   {0, 0}, keypress_c,   "CaB"_ts, {1, 0} },
 
-    text_area_.on_redraw.connect(
-        [&cvs, this](auto const &regions)
-        {
-            for (auto const &region : regions)
-            {
-                munin::render_surface surface{cvs};
-                text_area_.draw(surface, region);
-            }
-        });
+    { "test"_ts, {4, 0}, keypress_bs,  "tes"_ts, {3, 0} },
+    { "test"_ts, {4, 0}, keypress_del, "tes"_ts, {3, 0} },
+};
 
-    auto cursor_position = terminalpp::point{};
-    text_area_.on_cursor_position_changed.connect(
-        [&cursor_position, this]()
-        {
-            cursor_position = text_area_.get_cursor_position();
-        });
-
-    text_area_.insert_text("test"_ts);
-    text_area_.set_caret_position(3);
-
-    auto const keypress_bs = terminalpp::virtual_key {
-        terminalpp::vk::bs,
-        terminalpp::vk_modifier::none,
-        1
-    };
-
-    text_area_.event(keypress_bs);
-
-    EXPECT_EQ("tes"_ts, text_area_.get_text());
-    EXPECT_EQ(terminalpp::point(2, 0), cursor_position);
-    EXPECT_EQ(terminalpp::element('t'), cvs[0][0]);
-    EXPECT_EQ(terminalpp::element('e'), cvs[1][0]);
-    EXPECT_EQ(terminalpp::element('s'), cvs[2][0]);
-    EXPECT_EQ(terminalpp::element(' '), cvs[3][0]);
-}
-
-TEST_F(a_text_area, deletes_text_before_the_cursor_when_del_is_pressed)
-{
-    auto cvs = terminalpp::canvas({10, 3});
-    text_area_.set_size({10, 3});
-
-    text_area_.on_redraw.connect(
-        [&cvs, this](auto const &regions)
-        {
-            for (auto const &region : regions)
-            {
-                munin::render_surface surface{cvs};
-                text_area_.draw(surface, region);
-            }
-        });
-
-    auto cursor_position = terminalpp::point{};
-    text_area_.on_cursor_position_changed.connect(
-        [&cursor_position, this]()
-        {
-            cursor_position = text_area_.get_cursor_position();
-        });
-
-    text_area_.insert_text("test"_ts);
-    text_area_.set_caret_position(3);
-
-    auto const keypress_del = terminalpp::virtual_key {
-        terminalpp::vk::del,
-        terminalpp::vk_modifier::none,
-        1
-    };
-
-    text_area_.event(keypress_del);
-
-    EXPECT_EQ("tes"_ts, text_area_.get_text());
-    EXPECT_EQ(terminalpp::point(2, 0), cursor_position);
-    EXPECT_EQ(terminalpp::element('t'), cvs[0][0]);
-    EXPECT_EQ(terminalpp::element('e'), cvs[1][0]);
-    EXPECT_EQ(terminalpp::element('s'), cvs[2][0]);
-    EXPECT_EQ(terminalpp::element(' '), cvs[3][0]);
-}
+INSTANTIATE_TEST_SUITE_P(
+    text_areas_handle_keypresses,
+    a_text_area_receiving_keypresses,
+    ValuesIn(keypress_data)
+);
