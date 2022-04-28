a7c5302aa7600bc2879b3277de1495e644555424
KazDragon <KazDragon@users.noreply.github.com>
2022-04-28 11:24:14 +0000

    deploy: f02ed59ec314402fc4e32833e4b20c2edaeb5478

--- search/functions_c.js
+++ search/functions_c.js
@@ -4,5 +4,5 @@ var searchData=
   ['redraw_5fcomponents_272',['redraw_components',['../classmunin_1_1animator.html#a51fe511d628b69156612197b129b6701',1,'munin::animator']]],
   ['remove_5fcomponent_273',['remove_component',['../classmunin_1_1container.html#a45da577fd96d70d39d3abdf3f6eb1bee',1,'munin::container']]],
   ['render_5fsurface_274',['render_surface',['../classmunin_1_1render__surface.html#a4590c8866d0cd4dfc394eb834dcd1704',1,'munin::render_surface']]],
-  ['repaint_275',['repaint',['../classmunin_1_1window.html#a061c95910b38ad8e349227b94997f3f6',1,'munin::window']]]
+  ['repaint_275',['repaint',['../classmunin_1_1window.html#a35421093f96a34d592cab1ec0f732f83',1,'munin::window']]]
 ];
