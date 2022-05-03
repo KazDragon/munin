e24f28fb729c39f71069acf521c4e1593150bfa3
KazDragon <KazDragon@users.noreply.github.com>
2022-05-03 09:54:44 +0000

    deploy: a5d7e7576e5cac415d4bd0c61b69840d77da0a54

--- search/all_11.js
+++ search/all_11.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['west_5fborder_5fwidth_140',['west_border_width',['../classmunin_1_1frame.html#a09845453bcbfb7a062089202ee68f556',1,'munin::frame::west_border_width()'],['../classmunin_1_1scroll__frame.html#a6d62c89fde087a223e51675d8a3f1984',1,'munin::scroll_frame::west_border_width()'],['../classmunin_1_1solid__frame.html#a248cb1fea3a617a7acfffb7da7fd64a6',1,'munin::solid_frame::west_border_width()'],['../classmunin_1_1titled__frame.html#ab997f5a0b4cd338b1f2fefa4e1216f57',1,'munin::titled_frame::west_border_width()']]],
-  ['window_141',['window',['../classmunin_1_1window.html',1,'munin::window'],['../classmunin_1_1window.html#ad86aa684ebff86acd89a19ae8238e040',1,'munin::window::window(std::shared_ptr&lt; component &gt; content)'],['../classmunin_1_1window.html#a1209cf58b9798ec3dee0624a5d5a7db8',1,'munin::window::window(std::shared_ptr&lt; component &gt; content, render_surface_capabilities const &amp;capabilities)']]],
+  ['window_141',['window',['../classmunin_1_1window.html',1,'munin::window'],['../classmunin_1_1window.html#a12c9a5b9ae459714eee5d2b1d8886f6a',1,'munin::window::window(terminalpp::terminal &amp;terminal, std::shared_ptr&lt; component &gt; content)'],['../classmunin_1_1window.html#a81c00c83150f89bf6c7186dc1fa24bfd',1,'munin::window::window(terminalpp::terminal &amp;terminal, std::shared_ptr&lt; component &gt; content, render_surface_capabilities const &amp;capabilities)']]],
   ['window_5fevent_5fdispatcher_142',['window_event_dispatcher',['../structwindow__event__dispatcher.html',1,'']]]
 ];
--- search/all_d.js
+++ search/all_d.js
@@ -7,6 +7,6 @@ var searchData=
   ['remove_5fcomponent_100',['remove_component',['../classmunin_1_1container.html#a45da577fd96d70d39d3abdf3f6eb1bee',1,'munin::container']]],
   ['render_5fsurface_101',['render_surface',['../classmunin_1_1render__surface.html',1,'munin::render_surface'],['../classmunin_1_1render__surface.html#a4590c8866d0cd4dfc394eb834dcd1704',1,'munin::render_surface::render_surface()']]],
   ['render_5fsurface_5fcapabilities_102',['render_surface_capabilities',['../structmunin_1_1render__surface__capabilities.html',1,'munin']]],
-  ['repaint_103',['repaint',['../classmunin_1_1window.html#a35421093f96a34d592cab1ec0f732f83',1,'munin::window']]],
+  ['repaint_103',['repaint',['../classmunin_1_1window.html#a7ba288ffb1005f640933c395ad0ea57a',1,'munin::window']]],
   ['resize_5fstrategy_104',['resize_strategy',['../structmunin_1_1viewport_1_1resize__strategy.html',1,'munin::viewport']]]
 ];
