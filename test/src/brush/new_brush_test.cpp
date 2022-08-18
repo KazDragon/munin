3e2c40f6e6f93dbd9c7b46395801e7e84202131f
Matthew Chaplain <KazDragon@users.noreply.github.com>
2022-08-18 07:37:31 +0000

    Simplified new_brush_test.

--- test/src/brush/new_brush_test.cpp
+++ test/src/brush/new_brush_test.cpp
@@ -1,7 +1,10 @@
+#include "similar_canvas.hpp"
 #include <munin/brush.hpp>
 #include <munin/render_surface.hpp>
 #include <terminalpp/algorithm/for_each_in_region.hpp>
 #include <gtest/gtest.h>
+
+using namespace terminalpp::literals;
 
 TEST(a_new_brush, has_a_singular_preferred_size)
 {
@@ -63,15 +66,11 @@ TEST(a_new_brush, draws_whitespace_on_the_canvas)
     munin::render_surface surface{canvas};
     brush.draw(surface, {{}, brush.get_size()});
 
-    ASSERT_EQ(terminalpp::element{' '}, canvas[0][0]);
-    ASSERT_EQ(terminalpp::element{' '}, canvas[0][1]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[0][2]);
-    ASSERT_EQ(terminalpp::element{' '}, canvas[1][0]);
-    ASSERT_EQ(terminalpp::element{' '}, canvas[1][1]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[1][2]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[2][0]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[2][1]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[2][2]);
+    assert_similar_canvas(
+        { "  X"_ts,
+          "  X"_ts,
+          "XXX"_ts },
+        canvas);
 }
 
 TEST(a_new_brush, refuses_focus)
@@ -105,41 +104,12 @@ TEST(a_new_brush_with_a_single_line_pattern, draws_that_pattern_repeatedly)
     surface.offset_by({1, 1});
     brush.draw(surface, {{}, brush.get_size()});
 
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[0][0]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[1][0]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[2][0]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[3][0]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[4][0]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[5][0]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[6][0]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[7][0]);
-
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[0][1]);
-    ASSERT_EQ(terminalpp::element{'a'}, canvas[1][1]);
-    ASSERT_EQ(terminalpp::element{'b'}, canvas[2][1]);
-    ASSERT_EQ(terminalpp::element{'c'}, canvas[3][1]);
-    ASSERT_EQ(terminalpp::element{'a'}, canvas[4][1]);
-    ASSERT_EQ(terminalpp::element{'b'}, canvas[5][1]);
-    ASSERT_EQ(terminalpp::element{'c'}, canvas[6][1]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[7][1]);
-
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[0][2]);
-    ASSERT_EQ(terminalpp::element{'a'}, canvas[1][2]);
-    ASSERT_EQ(terminalpp::element{'b'}, canvas[2][2]);
-    ASSERT_EQ(terminalpp::element{'c'}, canvas[3][2]);
-    ASSERT_EQ(terminalpp::element{'a'}, canvas[4][2]);
-    ASSERT_EQ(terminalpp::element{'b'}, canvas[5][2]);
-    ASSERT_EQ(terminalpp::element{'c'}, canvas[6][2]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[7][2]);
-
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[0][3]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[1][3]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[2][3]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[3][3]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[4][3]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[5][3]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[6][3]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[7][3]);
+    assert_similar_canvas(
+        { "XXXXXXXX"_ts,
+          "XabcabcX"_ts,
+          "XabcabcX"_ts,
+          "XXXXXXXX"_ts },
+        canvas);
 }
 
 TEST(a_new_brush_with_a_multi_line_pattern, draws_that_pattern_repeatedly)
@@ -169,47 +139,14 @@ TEST(a_new_brush_with_a_multi_line_pattern, draws_that_pattern_repeatedly)
     surface.offset_by({1, 1});
     brush.draw(surface, {{}, brush.get_size()});
 
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[0][0]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[1][0]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[2][0]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[3][0]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[4][0]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[5][0]);
-
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[0][1]);
-    ASSERT_EQ(terminalpp::element{'a'}, canvas[1][1]);
-    ASSERT_EQ(terminalpp::element{'b'}, canvas[2][1]);
-    ASSERT_EQ(terminalpp::element{'a'}, canvas[3][1]);
-    ASSERT_EQ(terminalpp::element{'b'}, canvas[4][1]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[5][1]);
-
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[0][2]);
-    ASSERT_EQ(terminalpp::element{'c'}, canvas[1][2]);
-    ASSERT_EQ(terminalpp::element{'d'}, canvas[2][2]);
-    ASSERT_EQ(terminalpp::element{'c'}, canvas[3][2]);
-    ASSERT_EQ(terminalpp::element{'d'}, canvas[4][2]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[5][2]);
-
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[0][3]);
-    ASSERT_EQ(terminalpp::element{'a'}, canvas[1][3]);
-    ASSERT_EQ(terminalpp::element{'b'}, canvas[2][3]);
-    ASSERT_EQ(terminalpp::element{'a'}, canvas[3][3]);
-    ASSERT_EQ(terminalpp::element{'b'}, canvas[4][3]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[5][3]);
-
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[0][4]);
-    ASSERT_EQ(terminalpp::element{'c'}, canvas[1][4]);
-    ASSERT_EQ(terminalpp::element{'d'}, canvas[2][4]);
-    ASSERT_EQ(terminalpp::element{'c'}, canvas[3][4]);
-    ASSERT_EQ(terminalpp::element{'d'}, canvas[4][4]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[5][4]);
-
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[0][5]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[1][5]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[2][5]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[3][5]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[4][5]);
-    ASSERT_EQ(terminalpp::element{'X'}, canvas[5][5]);
+    assert_similar_canvas(
+        { "XXXXXX"_ts,
+          "XababX"_ts,
+          "XcdcdX"_ts,
+          "XababX"_ts,
+          "XcdcdX"_ts,
+          "XXXXXX"_ts },
+        canvas);
 }
 
 TEST(make_brush_with_no_pattern, makes_a_new_default_brush)
