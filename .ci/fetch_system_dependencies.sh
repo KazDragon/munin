861de544225c444a0b13745e93e0938e6f2da093
Matthew Chaplain <matthew.chaplain@gmail.com>
2024-04-01 07:32:31 +0000

    Add dockerfile for VSCode/WSL development

--- .ci/fetch_system_dependencies.sh
+++ .ci/fetch_system_dependencies.sh
@@ -32,7 +32,17 @@ if [ ! -f "$EXTERNAL_ROOT/include/terminalpp-3.1.0/terminalpp/version.hpp" ]; th
     wget https://github.com/KazDragon/terminalpp/archive/v3.1.0.tar.gz;
     tar -xzf v3.1.0.tar.gz;
     cd terminalpp-3.1.0;
-    cmake -DCMAKE_INSTALL_PREFIX="$EXTERNAL_ROOT" -DCMAKE_PREFIX_PATH="$EXTERNAL_ROOT" -DTERMINALPP_WITH_TESTS=False -DTERMINALPP_VERSION="3.0.2" .;
+    cmake -DCMAKE_INSTALL_PREFIX="$EXTERNAL_ROOT" -DCMAKE_PREFIX_PATH="$EXTERNAL_ROOT" -DTERMINALPP_WITH_TESTS=False -DTERMINALPP_VERSION="3.1.0" .;
+    make -j2 && make install;
+    cd ..;
+fi
+
+# Install Console++ dependency
+if [ ! -f "$EXTERNAL_ROOT/include/consolepp-0.1.1/consolepp/version.hpp" ]; then
+    wget https://github.com/KazDragon/consolepp/archive/v0.1.1.tar.gz;
+    tar -xzf v0.1.1.tar.gz;
+    cd consolepp-0.1.1;
+    cmake -DCMAKE_INSTALL_PREFIX="$EXTERNAL_ROOT" -DCMAKE_PREFIX_PATH="$EXTERNAL_ROOT" -DCONSOLEPP_WITH_TESTS=False .;
     make -j2 && make install;
     cd ..;
 fi
