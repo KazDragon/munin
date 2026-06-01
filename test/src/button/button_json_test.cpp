888470d4e9ae1c457c566af970be82012f4f63fc
PerdixOfMars <perdix.p76l@googlemail.com>
2026-06-01 14:53:55 +0000

    Add button JSON coverage

--- test/src/button/button_json_test.cpp
+++ test/src/button/button_json_test.cpp
@@ -21,3 +21,48 @@ TEST(a_button, reports_its_visible_text_as_accessible_name)
 
     ASSERT_EQ("Cancel", json["name"]);
 }
+
+TEST(a_button, reports_its_current_bounds_as_json)
+{
+    munin::button button("OK");
+    button.set_position({3, 5});
+    button.set_size({7, 2});
+
+    nlohmann::json json = button.to_json();
+
+    ASSERT_EQ(3, json["position"]["x"]);
+    ASSERT_EQ(5, json["position"]["y"]);
+    ASSERT_EQ(7, json["size"]["width"]);
+    ASSERT_EQ(2, json["size"]["height"]);
+}
+
+TEST(a_button, reports_its_preferred_size_as_json)
+{
+    munin::button button("OK");
+
+    nlohmann::json json = button.to_json();
+
+    ASSERT_EQ(4, json["preferred_size"]["width"]);
+    ASSERT_EQ(3, json["preferred_size"]["height"]);
+}
+
+TEST(a_button, reports_when_it_has_focus_as_json)
+{
+    munin::button button("OK");
+    button.set_focus();
+
+    nlohmann::json json = button.to_json();
+
+    ASSERT_EQ(true, json["has_focus"]);
+}
+
+TEST(a_button, reports_its_cursor_state_as_json)
+{
+    munin::button button("OK");
+
+    nlohmann::json json = button.to_json();
+
+    ASSERT_EQ(false, json["cursor_state"]);
+    ASSERT_EQ(0, json["cursor_position"]["x"]);
+    ASSERT_EQ(0, json["cursor_position"]["y"]);
+}
