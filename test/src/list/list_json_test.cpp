077fa54fa2dc85bb3ff67b086c13885c30c437f2
PerdixOfMars <perdix.p76l@googlemail.com>
2026-06-03 11:16:05 +0000

    Refactor list JSON tests

--- test/src/list/list_json_test.cpp
+++ test/src/list/list_json_test.cpp
@@ -3,70 +3,71 @@
 
 using namespace terminalpp::literals;  // NOLINT
 
-TEST(a_default_list, reports_its_type_as_json)
+class a_list_json_snapshot : public testing::Test
 {
-    munin::list list;
+protected:
+    [[nodiscard]] nlohmann::json current_json() const
+    {
+        return list_.to_json();
+    }
 
-    auto const json = list.to_json();
+    munin::list list_;
+};
+
+TEST_F(a_list_json_snapshot, reports_its_type)
+{
+    auto const json = current_json();
 
     ASSERT_EQ("list", json.at("type"));
 }
 
-TEST(a_default_list, reports_empty_items_as_json)
+TEST_F(a_list_json_snapshot, reports_empty_items)
 {
-    munin::list list;
-
-    auto const json = list.to_json();
+    auto const json = current_json();
 
     ASSERT_EQ(0, json.at("items").at("size"));
 }
 
-TEST(a_list_json_with_an_item, reports_its_item_count_as_json)
+TEST_F(a_list_json_snapshot, reports_item_count)
 {
-    munin::list list;
-    list.set_items({"first"_ts});
+    list_.set_items({"first"_ts});
 
-    auto const json = list.to_json();
+    auto const json = current_json();
 
     ASSERT_EQ(1, json.at("items").at("size"));
 }
 
-TEST(a_list_json_with_an_item, reports_its_item_content_as_json)
+TEST_F(a_list_json_snapshot, reports_item_content)
 {
-    munin::list list;
-    list.set_items({"first"_ts});
+    list_.set_items({"first"_ts});
 
-    auto const json = list.to_json();
+    auto const json = current_json();
 
     ASSERT_EQ("first", json.at("items").at("content").at(0));
 }
 
-TEST(a_list_json_with_two_items, reports_the_second_item_content_as_json)
+TEST_F(a_list_json_snapshot, reports_the_second_item_content)
 {
-    munin::list list;
-    list.set_items({"first"_ts, "second"_ts});
+    list_.set_items({"first"_ts, "second"_ts});
 
-    auto const json = list.to_json();
+    auto const json = current_json();
 
     ASSERT_EQ("second", json.at("items").at("content").at(1));
 }
 
-TEST(a_default_list, reports_no_selected_item_as_json)
+TEST_F(a_list_json_snapshot, reports_no_selected_item)
 {
-    munin::list list;
-
-    auto const json = list.to_json();
+    auto const json = current_json();
 
     ASSERT_TRUE(json.at("selected_item_index").is_null());
 }
 
-TEST(a_list_json_with_a_selected_item, reports_the_selected_item_index_as_json)
+TEST_F(a_list_json_snapshot, reports_the_selected_item_index)
 {
-    munin::list list;
-    list.set_items({"first"_ts, "second"_ts});
-    list.select_item(1);
+    list_.set_items({"first"_ts, "second"_ts});
+    list_.select_item(1);
 
-    auto const json = list.to_json();
+    auto const json = current_json();
 
     ASSERT_EQ(1, json.at("selected_item_index"));
 }
