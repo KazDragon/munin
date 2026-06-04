a5a15bfc8b43158f54c70f5ef7d8311dddbbcb8e
PerdixOfMars <perdix.p76l@googlemail.com>
2026-06-04 08:51:53 +0000

    Cover brush default pattern reset JSON

--- test/src/brush/brush_json_test.cpp
+++ test/src/brush/brush_json_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <munin/brush.hpp>
 
+using namespace terminalpp::literals;  // NOLINT
+
 TEST(a_default_brush, reports_attributes_as_json)
 {
     munin::brush brush;
@@ -30,4 +32,15 @@ TEST(a_multi_line_brush, reports_attributes_as_json)
     ASSERT_EQ(2, json["pattern"]["size"]);
     ASSERT_EQ("test", json["pattern"]["content"][0]);
     ASSERT_EQ("success", json["pattern"]["content"][1]);
+}
+
+TEST(a_brush_whose_pattern_is_reset_to_default, reports_attributes_as_json)
+{
+    munin::brush brush("test"_ts);
+
+    brush.set_pattern();
+
+    nlohmann::json json = brush.to_json();
+    ASSERT_EQ(1, json["pattern"]["size"]);
+    ASSERT_EQ(" ", json["pattern"]["content"][0]);
 }
