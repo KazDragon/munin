6cce5923f898b8e61865b8a3c9b0450148ad41d6
Perdix of Mars <perdix.p76l@gmail.com>
2026-06-13 17:14:14 +0200

    Cover other brush json test

--- test/src/brush/brush_json_test.cpp
+++ test/src/brush/brush_json_test.cpp
@@ -44,3 +44,14 @@ TEST(a_brush_whose_pattern_is_reset_to_default, reports_attributes_as_json)
     ASSERT_EQ(1, json["pattern"]["size"]);
     ASSERT_EQ(" ", json["pattern"]["content"][0]);
 }
+
+TEST(a_brush_whose_pattern_is_set_to_a_single_line, reports_attributes_as_json)
+{
+    munin::brush brush;
+
+    brush.set_pattern("test"_ts);
+
+    nlohmann::json json = brush.to_json();
+    ASSERT_EQ(1, json["pattern"]["size"]);
+    ASSERT_EQ("test", json["pattern"]["content"][0]);
+}
