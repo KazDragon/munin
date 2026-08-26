0e0b2a74fe7658e5aacd99f3650cdb98adcda803
Perdix of Mars <perdix.p76l@gmail.com>
2026-08-26 07:11:32 +0000

    Cover nested synthesized button clicks

--- test/src/window/window_test.cpp
+++ test/src/window/window_test.cpp
@@ -2,6 +2,7 @@
 
 #include <gtest/gtest.h>
 #include <munin/button.hpp>
+#include <munin/container.hpp>
 #include <munin/event_context.hpp>
 
 using testing::_;
@@ -65,6 +66,43 @@ TEST(
     auto button = std::make_shared<munin::button>(" OK ");
     button->set_size({6, 3});
     auto window = munin::window(terminal, button);
+
+    auto click_count = 0;
+    button->on_click.connect([&click_count] { ++click_count; });
+
+    window.event(terminalpp::mouse::event{
+        .action_ = terminalpp::mouse::event_type::left_button_down,
+        .position_ = {1, 1},
+        .button_ = terminalpp::mouse::button::left
+    });
+
+    ASSERT_EQ(0, click_count);
+
+    window.event(terminalpp::mouse::event{
+        .action_ = terminalpp::mouse::event_type::button_up,
+        .position_ = {1, 1},
+        .button_ = terminalpp::mouse::button::left
+    });
+
+    ASSERT_EQ(1, click_count);
+}
+
+TEST(
+    a_window_mouse_events,
+    clicks_a_nested_button_on_left_release_when_clicks_can_be_synthesized)
+{
+    auto behaviour = terminalpp::behaviour{};
+    behaviour.supports_basic_mouse_tracking = true;
+    behaviour.supports_sgr_mouse_encoding = true;
+
+    fake_channel channel;
+    terminalpp::terminal terminal{channel, behaviour};
+    auto container = munin::make_container();
+    container->set_size({10, 5});
+    auto button = std::make_shared<munin::button>(" OK ");
+    button->set_size({6, 3});
+    container->add_component(button);
+    auto window = munin::window(terminal, container);
 
     auto click_count = 0;
     button->on_click.connect([&click_count] { ++click_count; });
